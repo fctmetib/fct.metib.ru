123c09b76ff95ebb3f5e5282fb60e5196331d26c
Lanceopries <35199929+Lanceopries@users.noreply.github.com>
2021-02-05 17:29:44 +0300

    feat(cabinet): real data was link with components

--- src/app/shared/layouts/protected-layout/header/header.component.ts
+++ src/app/shared/layouts/protected-layout/header/header.component.ts
@@ -3,8 +3,9 @@ import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 
-import { currentUserFactoringSelector } from 'src/app/auth/store/selectors';
+import { currentUserFactoringSelector, currentUserGeneralSelector } from 'src/app/auth/store/selectors';
 import { CurrentUserFactoringInterface } from '../../../types/currentUserFactoring.interface';
+import { CurrentUserGeneralInterface } from 'src/app/shared/types/currentUserGeneral.interface';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -13,10 +14,12 @@ import { CurrentUserFactoringInterface } from '../../../types/currentUserFactori
 export class HeaderComponent implements OnInit {
   items: MenuItem[];
   public currentUserFactoring$: Observable<CurrentUserFactoringInterface | null>;
+  public currentUser$: Observable<CurrentUserGeneralInterface | null>;
 
   constructor(private store: Store) {}
 
   ngOnInit() {
     this.currentUserFactoring$ = this.store.pipe(select(currentUserFactoringSelector));
+    this.currentUser$ = this.store.pipe(select(currentUserGeneralSelector));
   }
 }
