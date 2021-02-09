054183a07df443c5ff8cc7e9303c91a908e9f5ca
Lanceopries <35199929+Lanceopries@users.noreply.github.com>
2021-02-09 15:11:16 +0300

    fix(requests): small fix

--- src/app/client/modules/requests/components/requests-page/requests-page.component.ts
+++ src/app/client/modules/requests/components/requests-page/requests-page.component.ts
@@ -37,35 +37,7 @@ export class RequestsPageComponent implements OnInit {
 
   ngOnInit() {
     this.initializeValues()
-    this.fetchFeed()
-      // this.customerService.getCustomersLarge().then(customers => {
-      //     this.customers = customers;
-      //     this.loading = false;
-
-      //     this.customers.forEach(customer => customer.date = new Date(customer.date));
-      // });
-
-      this.representatives = [
-          {name: "Amy Elsner", image: 'amyelsner.png'},
-          {name: "Anna Fali", image: 'annafali.png'},
-          {name: "Asiya Javayant", image: 'asiyajavayant.png'},
-          {name: "Bernardo Dominic", image: 'bernardodominic.png'},
-          {name: "Elwin Sharvill", image: 'elwinsharvill.png'},
-          {name: "Ioni Bowcher", image: 'ionibowcher.png'},
-          {name: "Ivan Magalhaes",image: 'ivanmagalhaes.png'},
-          {name: "Onyama Limba", image: 'onyamalimba.png'},
-          {name: "Stephen Shaw", image: 'stephenshaw.png'},
-          {name: "Xuxue Feng", image: 'xuxuefeng.png'}
-      ];
-
-      this.statuses = [
-          {label: 'Unqualified', value: 'unqualified'},
-          {label: 'Qualified', value: 'qualified'},
-          {label: 'New', value: 'new'},
-          {label: 'Negotiation', value: 'negotiation'},
-          {label: 'Renewal', value: 'renewal'},
-          {label: 'Proposal', value: 'proposal'}
-      ]
+    this.fetch()
   }
 
 
@@ -80,7 +52,7 @@ export class RequestsPageComponent implements OnInit {
     this.isLoading$ = this.store.pipe(select(isLoadingSelector))
   }
 
-  fetchFeed(): void {
+  fetch(): void {
     this.store.dispatch(getRequestsAction())
   }
 }
