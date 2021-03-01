704b86d483e90974c28b01bf2fc9313a2af34aa2
Lanceopries <35199929+Lanceopries@users.noreply.github.com>
2021-03-01 20:30:28 +0300

    feat(freeduty): page updated

--- src/app/client/modules/freeduty/components/freeduty-page/freeduty-page.component.ts
+++ src/app/client/modules/freeduty/components/freeduty-page/freeduty-page.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { select, Store } from '@ngrx/store';
 import { Component, OnInit } from '@angular/core';
@@ -6,22 +7,34 @@ import { DutyService } from 'src/app/shared/services/share/duty.service';
 import { DatePipe } from '@angular/common';
 import { DutyFilterRequestInterface } from 'src/app/shared/types/duty/duty-filter-request.interface';
 import { getFreedutyAction } from '../../store/actions/getFreeduty.action';
-import { errorSelector, freedutySelector, isLoadingSelector } from '../../store/selectors';
+import {
+  errorSelector,
+  freedutySelector,
+  isLoadingSelector,
+} from '../../store/selectors';
 import { DutyInterface } from 'src/app/shared/types/duty/duty.interface';
 
 @Component({
   selector: 'app-freeduty-page',
   templateUrl: './freeduty-page.component.html',
   styleUrls: ['./freeduty-page.component.scss'],
-  providers: [DatePipe]
+  providers: [DatePipe],
 })
 export class FreedutyPageComponent implements OnInit {
   freeduty$: Observable<DutyInterface[] | null>;
   error$: Observable<string | null>;
   isLoading$: Observable<boolean>;
 
+  selectedItems: DutyInterface[];
+  filterForm: FormGroup;
+  filterDialog: boolean = false;
 
-  constructor(private store: Store, public dialogService: DialogService, private dutyService: DutyService, public datepipe: DatePipe) {}
+  constructor(
+    private store: Store,
+    private fb: FormBuilder,
+    public dialogService: DialogService,
+    public datepipe: DatePipe
+  ) {}
 
   ngOnInit() {
     this.initializeValues();
@@ -33,20 +46,38 @@ export class FreedutyPageComponent implements OnInit {
     this.error$ = this.store.pipe(select(errorSelector));
     this.isLoading$ = this.store.pipe(select(isLoadingSelector));
 
+    this.filterForm = this.fb.group({
+      dateFrom: [new Date(), [Validators.required]],
+      dateTo: [new Date(), [Validators.required]],
+    });
   }
 
-  updateData(): void {
+  openDateModal() {
+    this.filterDialog = true;
+  }
+
+  closeDateModal() {
+    this.filterDialog = false;
+  }
+
+  saveFilter() {
+    this.closeDateModal();
+  }
+
+  applyFilters(): void {
     this.fetch();
   }
 
   fetch(): void {
-    let data: DutyFilterRequestInterface = {
-      CustomerID: 1050,
-      DateFrom: new Date(),
-      DateTo: new Date(),
-      Free: true
-    }
+    if(this.filterForm.value.dateFrom && this.filterForm.value.dateTo) {
+      let data: DutyFilterRequestInterface = {
+        CustomerID: 1050,
+        DateFrom: new Date(this.filterForm.value.dateFrom),
+        DateTo:  new Date(this.filterForm.value.dateTo),
+        Free: false,
+      };
 
-    this.store.dispatch(getFreedutyAction({data}));
+      this.store.dispatch(getFreedutyAction({ data }));
+    }
   }
 }
