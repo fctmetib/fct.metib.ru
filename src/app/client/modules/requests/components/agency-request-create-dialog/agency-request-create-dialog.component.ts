6ef9730d6aaa499c39949f389b5a148a9d161cff
Lanceopries <35199929+Lanceopries@users.noreply.github.com>
2021-05-06 15:46:37 +0300

    feat(agency-request): rework in progress

--- src/app/client/modules/requests/components/agency-request-create-dialog/agency-request-create-dialog.component.ts
+++ src/app/client/modules/requests/components/agency-request-create-dialog/agency-request-create-dialog.component.ts
@@ -6,6 +6,7 @@ import { ChangeDetectorRef, Component } from '@angular/core';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { FileService } from 'src/app/shared/services/common/file.service';
 import { CommonService } from 'src/app/shared/services/common/common.service';
+import { ClientShipmentInterface } from 'src/app/shared/types/client/client-shipment.interface';
 
 @Component({
   selector: 'app-agency-request-create-dialog',
@@ -18,9 +19,16 @@ export class AgencyRequestCreateDialogComponent {
 
   public deliveries: DeliveryInterface[] = [];
 
+  public maxDate = new Date();
+
   //#region dynamic variables
   public btnSubmitValue = 'Создать';
   //#endregion
+
+  public addDeliveryDialog: boolean;
+  public currentShipmentID = null;
+  public shipments: ClientShipmentInterface[] = [];
+  public selectedShipments: ClientShipmentInterface[] = [];
 
   constructor(
     public ref: DynamicDialogRef,
@@ -51,6 +59,77 @@ export class AgencyRequestCreateDialogComponent {
 
   public addRequest(): void {
     this.requests.push(this.fb.control(''));
+  }
+
+
+  openNew(isEdit: boolean) {
+    if (isEdit) {
+      let shipment = this.selectedShipments[0];
+
+      if (shipment) {
+        this.currentShipmentID = shipment.ID;
+        this.shipmentForm.patchValue({
+          accountNumber: shipment.AccountNumber || '',
+          accountDate: new Date(shipment.AccountDate),
+          invoiceNumber: shipment.InvoiceNumber,
+          invoiceDate: new Date(shipment.InvoiceDate),
+          dateShipment: new Date(shipment.DateShipment),
+          summ: shipment.Summ,
+        });
+      } else {
+        return;
+      }
+    }
+    this.addDeliveryDialog = true;
+  }
+
+  hideDialog() {
+    this.addDeliveryDialog = false;
+  }
+
+  deleteShipments() {
+    this.selectedShipments.forEach((selectedShipment) => {
+      this.shipments.splice(this.shipments.indexOf(selectedShipment), 1);
+    });
+    this.selectedShipments = [];
+  }
+
+  addShipment() {
+    let shipment: ClientShipmentInterface = {
+      AccountNumber: this.shipmentForm.value.accountNumber,
+      AccountDate: this.shipmentForm.value.accountDate,
+      InvoiceNumber: this.shipmentForm.value.invoiceNumber,
+      InvoiceDate: this.shipmentForm.value.invoiceDate,
+      WaybillNumber: null,
+      WaybillDate: null,
+      DateShipment: this.shipmentForm.value.dateShipment,
+      DatePayment: null,
+      SummToFactor: null,
+      Summ: this.shipmentForm.value.summ,
+      ID: Math.floor(Math.random() * 100),
+    };
+
+    if (this.currentShipmentID) {
+      shipment.ID = this.currentShipmentID;
+      let shipmentIndex = this.shipments.indexOf(
+        this.shipments.find((x) => x.ID === this.currentShipmentID)
+      );
+      this.shipments[shipmentIndex] = shipment;
+      this.currentShipmentID = null;
+    } else {
+      this.shipments.push(shipment);
+    }
+
+    this.shipmentForm.reset();
+    this.hideDialog();
+  }
+
+  getShipmentsSum(items: ClientShipmentInterface[]) {
+    if(items) {
+    return items.reduce((sum, current) =>
+      sum + current.Summ, 0
+    )
+    }
   }
 
   get requests() {
