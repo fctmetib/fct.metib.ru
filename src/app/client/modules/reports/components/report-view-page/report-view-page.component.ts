f403ab086428006053f1427b9a5ac32213e1c8ac
Lanceopries <35199929+Lanceopries@users.noreply.github.com>
2021-05-12 18:28:17 +0300

    feat(report): report export in progress

--- src/app/client/modules/reports/components/report-view-page/report-view-page.component.ts
+++ src/app/client/modules/reports/components/report-view-page/report-view-page.component.ts
@@ -37,6 +37,32 @@ export class ReportViewPageComponent implements OnInit {
     });
   }
 
+  exportExcel() {
+    import("xlsx").then(xlsx => {
+      const worksheet = xlsx.utils.json_to_sheet(this.reportData, {
+
+      });
+      const workbook = { Sheets: { data: worksheet }, SheetNames: ["data"] };
+      const excelBuffer: any = xlsx.write(workbook, {
+        bookType: "xlsx",
+        type: "array"
+      });
+      this.saveAsExcelFile(excelBuffer, "отчет");
+    });
+  }
+
+
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    import("file-saver").then(FileSaver => {
+        let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+        let EXCEL_EXTENSION = '.xlsx';
+        const data: Blob = new Blob([buffer], {
+            type: EXCEL_TYPE
+        });
+        FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+    });
+}
+
   private prepareTable(): void {
     this.reportConfig = ReportType.getType(this.data.type)
     console.log(this.reportConfig)
