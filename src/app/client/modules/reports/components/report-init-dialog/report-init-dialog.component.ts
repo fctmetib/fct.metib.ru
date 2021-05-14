dbdcb4763083297c7eeb06678c21d958c7b532b4
Lanceopries <35199929+Lanceopries@users.noreply.github.com>
2021-05-14 12:04:18 +0300

    feat(report): updates for report generate

--- src/app/client/modules/reports/components/report-init-dialog/report-init-dialog.component.ts
+++ src/app/client/modules/reports/components/report-init-dialog/report-init-dialog.component.ts
@@ -3,7 +3,12 @@ import { MibArray } from './../../../../../shared/classes/arrays/mib-array.class
 import { Component } from '@angular/core';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { ControlConfigReportInterface } from '../../types/common/control-config.interface';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { DeliveryService } from 'src/app/shared/services/share/delivery.service';
 import { DeliveryInterface } from 'src/app/shared/types/delivery/delivery.interface';
 
@@ -23,11 +28,14 @@ export class ReportInitDialogComponent {
   public uniqDebtors: CounterpartyReferenceInterface[] = [];
   public isEmpty: boolean;
 
+  public statusShipments: any = [];
+  public createdByList: any = [];
+
   constructor(
     public ref: DynamicDialogRef,
     public config: DynamicDialogConfig,
     private fb: FormBuilder,
-    private deliveryService: DeliveryService,
+    private deliveryService: DeliveryService
   ) {}
 
   ngOnInit() {
@@ -36,7 +44,7 @@ export class ReportInitDialogComponent {
   }
 
   public onSubmit(): void {
-    console.log(this.form.value)
+    console.log(this.form.value);
     this.ref.close(this.form.value);
   }
 
@@ -49,67 +57,112 @@ export class ReportInitDialogComponent {
     this.controlConfig = this.currentData.config;
     console.log(this.currentData);
 
-    this.deliveryService.getDeliveriesWithStats().subscribe(resp => {
-      let debtors = resp.map(x => x.Debtor);
+    this.deliveryService.getDeliveriesWithStats().subscribe((resp) => {
+      let debtors = resp.map((x) => x.Debtor);
       debtors.push({
         Title: 'Все',
-        ID: 0
+        ID: 0,
       });
       debtors.reverse();
 
       this.uniqDebtors = MibArray.getUniqByProperty(debtors, 'Title');
-      console.log(this.uniqDebtors)
-    })
+      console.log(this.uniqDebtors);
+    });
+
+    this.createdByList = [
+      {
+        ID: 'me',
+        Title: 'Только созданные мной',
+      },
+      {
+        ID: 'all',
+        Title: 'Все отчеты организации',
+      },
+    ];
+
+    this.statusShipments = [
+      {
+        ID: 'undone',
+        Title: 'Незакрытые накладные',
+      },
+      {
+        ID: 'all',
+        Title: 'Все накладные',
+      },
+    ];
   }
 
   private initForm(): void {
     this.form = this.fb.group({});
 
     // let dateFrom = new Date();
-    //TODO: init протокол отчетов .
 
-    if(this.controlConfig.isEmpty) {
+    if (this.controlConfig.isEmpty) {
       this.isEmpty = true;
     }
 
-    if(this.controlConfig.isDateFrom) {
-      this.form.addControl('dateFrom', new FormControl('', Validators.required))
+    if (this.controlConfig.isDateFrom) {
+      this.form.addControl(
+        'dateFrom',
+        new FormControl('', Validators.required)
+      );
     }
 
-    if(this.controlConfig.isDateTo) {
-      this.form.addControl('dateTo', new FormControl('', Validators.required))
+    if (this.controlConfig.isDateTo) {
+      this.form.addControl('dateTo', new FormControl('', Validators.required));
     }
 
-    if(this.controlConfig.isDebitor) {
-      this.form.addControl('debitor', new FormControl('', Validators.required))
+    if (this.controlConfig.isDebitor) {
+      this.form.addControl('debitor', new FormControl('', Validators.required));
     }
 
-    if(this.controlConfig.isOnDate) {
-      this.form.addControl('onDate', new FormControl(Date, Validators.required))
+    if (this.controlConfig.isOnDate) {
+      this.form.addControl(
+        'onDate',
+        new FormControl(Date, Validators.required)
+      );
     }
 
-    if(this.controlConfig.isStatusRequest) {
-      this.form.addControl('statusRequest', new FormControl('', Validators.required))
+    if (this.controlConfig.isStatusRequest) {
+      this.form.addControl(
+        'statusRequest',
+        new FormControl('', Validators.required)
+      );
     }
 
-    if(this.controlConfig.isDaysDelay) {
-      this.form.addControl('daysDelay', new FormControl('', Validators.required))
+    if (this.controlConfig.isDaysDelay) {
+      this.form.addControl(
+        'daysDelay',
+        new FormControl(0, Validators.required)
+      );
     }
 
-    if(this.controlConfig.isNumberOrder) {
-      this.form.addControl('numberOrder', new FormControl('', Validators.required))
+    if (this.controlConfig.isNumberOrder) {
+      this.form.addControl(
+        'numberOrder',
+        new FormControl('', Validators.required)
+      );
     }
 
-    if(this.controlConfig.isNumberRequest) {
-      this.form.addControl('numberRequest', new FormControl('', Validators.required))
+    if (this.controlConfig.isNumberRequest) {
+      this.form.addControl(
+        'numberRequest',
+        new FormControl('', Validators.required)
+      );
     }
 
-    if(this.controlConfig.isSelectReportDropdown) {
-      this.form.addControl('selectReportDropdown', new FormControl('', Validators.required))
+    if (this.controlConfig.isSelectReportDropdown) {
+      this.form.addControl(
+        'selectReportDropdown',
+        new FormControl('', Validators.required)
+      );
     }
 
-    if(this.controlConfig.isSelectReportCheckbox) {
-      this.form.addControl('selectReportCheckbox', new FormControl('', Validators.required))
+    if (this.controlConfig.isSelectReportCheckbox) {
+      this.form.addControl(
+        'selectReportCheckbox',
+        new FormControl(true, Validators.required)
+      );
     }
   }
 }
