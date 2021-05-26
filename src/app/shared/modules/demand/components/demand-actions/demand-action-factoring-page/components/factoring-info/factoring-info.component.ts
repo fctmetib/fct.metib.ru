e5fabe996d74cb0a3fef9ffc803ac338a9823d37
Lanceopries <35199929+Lanceopries@users.noreply.github.com>
2021-05-26 11:16:50 +0300

    feat(demand): factoring was add

--- src/app/shared/modules/demand/components/demand-actions/demand-action-factoring-page/components/factoring-info/factoring-info.component.ts
+++ src/app/shared/modules/demand/components/demand-actions/demand-action-factoring-page/components/factoring-info/factoring-info.component.ts
@@ -1,9 +1,17 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from "@angular/core";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-factoring-info',
   templateUrl: './factoring-info.component.html',
-  styleUrls: ['./factoring-info.component.scss']
+  styleUrls: ['./factoring-info.component.scss'],
 })
 export class FactoringInfoComponent implements OnInit, OnDestroy {
   @Input()
@@ -12,9 +20,24 @@ export class FactoringInfoComponent implements OnInit, OnDestroy {
   @Output()
   sendMessage: EventEmitter<any> = new EventEmitter<any>();
 
+  items: MenuItem[];
+
   ngOnInit() {
+    this.items = [
+      {
+        label: 'Заявка',
+      },
+      {
+        label: 'Лимит',
+      },
+      {
+        label: 'Досье',
+      },
+      {
+        label: 'Факторинг',
+      },
+    ];
   }
 
-  ngOnDestroy(): void {
-  }
+  ngOnDestroy(): void {}
 }
