3c383ec285e2ab4292207f14c04c187d775addd8
Lanceopries <starwarsQ12@gmail.com>
2021-07-29 15:42:20 +0300

    fix(public):create organizartion.service/interface

---
+++ src/app/public/service/organization.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { OrganizationInterface } from '../type/organization.interface';
+import { ClientRequestInterface } from '../../shared/types/client/client-request.interface';
+import { environment } from 'src/environments/environment';
+
+@Injectable()
+export class OrganizationService {
+  constructor(private http: HttpClient) {}
+
+  send(data: OrganizationInterface): Observable<OrganizationInterface> {
+    const url = `public/anket/factoring/request`;
+    return this.http.post<OrganizationInterface>(url, data);
+  }
+
+}
---
+++ src/app/public/type/organization.interface.ts
@@ -0,0 +1,10 @@
+export interface OrganizationInterface {
+    Organization: string;
+    Person: string;
+    Phone: string;
+    Email: string;
+    INN: string;
+    Comment: string;
+    Agreement: boolean;
+  }
+  
