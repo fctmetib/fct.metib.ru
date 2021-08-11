e3def1b8f4b2469f9b47507b1ce53039e5db2fdf
Lanceopries <35199929+Lanceopries@users.noreply.github.com>
2021-08-11 14:14:26 +0300

    feat(public): public side improvments in progress

--- src/app/public/shared/mib-sections/mib-section-header/mib-section-header.component.ts
+++ src/app/public/shared/mib-sections/mib-section-header/mib-section-header.component.ts
@@ -2,9 +2,11 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mib-section-header',
+  styleUrls: ['./mib-section-header.component.scss'],
   template: `
     <div
       class="mib-section mib-section__header white lg"
+      [ngClass]="pageType"
       [ngStyle]="{ 'background-image': 'url(' + backgroundImageUrl + ')'}"
     >
       <div class="mib-container">
@@ -16,6 +18,9 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MibSectionHeaderComponent implements OnInit {
   @Input()
   backgroundImageUrl: string;
+
+  @Input()
+  pageType: string;
 
   constructor() {}
 
