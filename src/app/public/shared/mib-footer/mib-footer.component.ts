fd70aa7ed0b23873a756b08bc403aa03f8f80276
Lanceopries <35199929+Lanceopries@users.noreply.github.com>
2021-08-17 17:24:17 +0300

    feat(public): public page improvments

--- src/app/public/shared/mib-footer/mib-footer.component.ts
+++ src/app/public/shared/mib-footer/mib-footer.component.ts
@@ -9,5 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class MibFooterComponent implements OnInit {
   constructor() { }
 
+  public openSocial(type: string) {
+    switch (type) {
+      //
+      case 'whatsapp':
+        window.open('https://wa.me/79259508870', '_blank');
+        break;
+      case 'instagram':
+        window.open('https://www.instagram.com/factoring.metib.ru/', '_blank');
+        break;
+    }
+  }
+
   ngOnInit() { }
 }
