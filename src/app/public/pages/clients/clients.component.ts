dcbb0b06ff8ab8dbaf3e5119bc73115ec274ceab
Lanceopries <35199929+Lanceopries@users.noreply.github.com>
2021-08-18 11:27:52 +0300

    feat(public): public page improved

--- src/app/public/pages/clients/clients.component.ts
+++ src/app/public/pages/clients/clients.component.ts
@@ -10,4 +10,17 @@ export class ClientsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() { }
+
+  public openAstralList() {
+    window.open('https://astral.ru/', '_blank');
+  }
+
+  public getContract() {
+    window.open(`${window.location.origin}/assets/_files/factoring_contract.zip`);
+  }
+
+  public getInstruction() {
+    window.open(`${window.location.origin}/assets/_files/FactorClientHelp.zip`);
+  }
+
 }
