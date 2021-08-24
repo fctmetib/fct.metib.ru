8065313b3f247bc5070087af88726b2dc2bd34d2
Lanceopries <35199929+Lanceopries@users.noreply.github.com>
2021-08-24 18:08:45 +0300

    fix(public): public fixes

--- src/app/public/pages/tariffs/tariffs.component.ts
+++ src/app/public/pages/tariffs/tariffs.component.ts
@@ -14,7 +14,7 @@ export class TariffsComponent implements OnInit, OnDestroy {
   private subscription$: Subscription = new Subscription();
 
   public financeForm = this.fb.group({
-    organization: [''],
+    organization: ['', Validators.required],
     name: ['', Validators.required],
     phone: ['', Validators.required],
     inn: ['', Validators.required],
