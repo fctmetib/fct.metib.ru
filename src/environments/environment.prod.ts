66a9127670133702edb6b614e6f98e392cb0f9e8
fctmetib <102676361+fctmetib@users.noreply.github.com>
2022-03-30 17:39:08 +0300

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  // new  https://api-factoring.metib.ru/
+  apiUrl: 'https://api-factoring.metib.ru/api/',
+  apiFileUploadUrl: 'https://api-factoring.metib.ru/api/',
+  // old
+  // apiUrl: 'https://api-factoring.metib.ru:8096/api/',
+  // apiFileUploadUrl: 'https://api-factoring.metib.ru:8096/api/',
+  // -----------------------------------------------------------
+  production: true,
+  limit: 10,
+  cryptoPrivateKey: 'gqpwro9-12itvnmzxn2j3tsvnsdnn21n209fasxz',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,30 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  // new prod
+  apiUrl: 'https://api-factoring.metib.ru/api/',
+  apiFileUploadUrl: 'https://api-factoring.metib.ru/api/',
+  // new dev
+  // apiUrl: 'http://api-factoring-test.metib.ru/api/',
+  // apiFileUploadUrl: 'http://api-factoring-test.metib.ru/api/',
+
+  // old
+  // apiUrl: 'http://api-factoring.metib.ru:8094/api/',
+  // apiFileUploadUrl: 'http://api-factoring.metib.ru:8094/api/',
+  // ------------------------------------------------------------
+  // apiUrl: 'https://simple-factoring.metib.ru/api',
+  limit: 10,
+  cryptoPrivateKey: 'gqpwro9-12itvnmzxn2j3tsvnsdnn21n209fasxz',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
