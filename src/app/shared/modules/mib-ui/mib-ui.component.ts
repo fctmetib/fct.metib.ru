696ca63151e911dcd22ae460af5f9d2ca87ad8cc
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-11-14 20:29:10 +0600

    feat: moved comonent scss prop, add btn styles

--- src/app/shared/modules/mib-ui/mib-ui.component.ts
+++ src/app/shared/modules/mib-ui/mib-ui.component.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core'
 @Component({
 	selector: 'app-mib-ui',
 	templateUrl: './mib-ui.component.html',
-	styles: []
+	styles: [
+		`
+			.btn-wrapper {
+				display: flex;
+				gap: 2rem;
+				flex-wrap: wrap;
+			}
+		`
+	]
 })
 export class MibUiComponent {
 	login() {
