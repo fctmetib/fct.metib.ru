b231adb61aed45a620d40a99d502e09ed1a8198b
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-11-17 21:07:21 +0600

    feat: add input basic styles, add/refactor global styles

--- src/app/shared/ui-kit/input/directives/metib-input.directive.ts
+++ src/app/shared/ui-kit/input/directives/metib-input.directive.ts
@@ -1,8 +1,38 @@
-import { Directive, ElementRef } from '@angular/core'
+import { Directive, ElementRef, HostBinding, Input } from '@angular/core'
+import {
+	InputCustom,
+	InputSize,
+	InputStatus,
+	InputType
+} from '../interfaces/input.interface'
 
 @Directive({
 	selector: '[mibInputDirective]'
 })
 export class MetibInputDirective {
 	constructor(public el: ElementRef<HTMLInputElement>) {}
+
+	@Input() disabled: boolean = false
+	@Input() controls: boolean = true
+	@Input() custom: InputCustom = ''
+	@Input() size: InputSize = 'l'
+	@Input() status?: InputStatus
+	@Input() class: string = ''
+	@Input() placeholder: string = ''
+	@Input() styleType: InputType = 'filled-secondary'
+	@Input() placeholderSuffix: string = ' *'
+
+	@HostBinding(`class`)
+	get getClasses() {
+		const classes: string[] = [
+			'input',
+			`input_${this.size}`,
+			`input_type-${this.styleType}`,
+			`input_${this.statusState}`,
+			...this.class.split(' ')
+		]
+		return classes.join(' ')
+	}
+
+	public statusState: InputStatus = 'default'
 }
--- src/app/shared/ui-kit/input/interfaces/input.interface.ts
+++ src/app/shared/ui-kit/input/interfaces/input.interface.ts
@@ -1,2 +1,10 @@
-export interface InputInterface {
-}
+export type InputSize = 'l' | 'm' | 's' | 'xs'
+export type InputType = 'filled-secondary' | 'floating'
+export type InputStatus =
+	| 'disabled'
+	| 'active'
+	| 'filled'
+	| 'error'
+	| 'default'
+	| 'hover'
+export type InputCustom = 'white' | ''
