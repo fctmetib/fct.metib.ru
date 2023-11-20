29837f2ccd6c4f79a7548011f745f72902f23b4f
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-11-20 22:26:54 +0600

    feat: add input eg, fix btn transition

--- src/app/shared/modules/mib-ui/mib-ui.component.ts
+++ src/app/shared/modules/mib-ui/mib-ui.component.ts
@@ -17,6 +17,71 @@ import { InputSize } from '../../ui-kit/input/interfaces/input.interface'
 				gap: 2rem;
 				flex-wrap: wrap;
 			}
+			.control {
+				display: flex;
+				min-height: 100%;
+				outline: none;
+				position: relative;
+				background-color: #0b1f351a;
+				padding: 16px;
+				max-width: 360px;
+				border-radius: 8px;
+			}
+
+			.icon-wrapper {
+				align-items: center;
+				display: flex;
+				flex-shrink: 0;
+				padding-right: 0;
+			}
+			.input-wrapper {
+				flex-grow: 1;
+				position: relative;
+			}
+			.input-wrapper:has(input:focus) .label-wrap > span {
+				transform: translateY(-70%);
+				transition: 0.2s;
+			}
+			.label-wrap {
+				align-items: center;
+				display: flex;
+				font-size: 16px;
+				font-weight: 400;
+				left: 16px;
+				line-height: 20px;
+				position: absolute;
+				right: 0;
+				transform: translateY(5%);
+			}
+			.label {
+				white-space: nowrap;
+				width: 100%;
+				font-size: 16px;
+				font-weight: 400;
+				line-height: 20px;
+				transition: 0.2s;
+			}
+			.input-wrap {
+				align-items: center;
+				display: flex;
+				height: 100%;
+				position: relative;
+			}
+			.input-wrap input {
+				appearance: none;
+				background: none;
+				border: 1px solid red;
+				box-sizing: border-box;
+				display: block;
+				font-size: 16px;
+				font-weight: 400;
+				height: 100%;
+				line-height: 20px;
+				margin: 0;
+				outline: none;
+				position: relative;
+				width: 100%;
+			}
 		`
 	]
 })
