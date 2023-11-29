6bfb2c06055ee92c398b06877ad436c22088e09a
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-11-29 20:46:25 +0600

    feat: add badge comp basic structure

--- src/app/shared/ui-kit/badge/badge.component.ts
+++ src/app/shared/ui-kit/badge/badge.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core'
+import { MibBadgeSize, MibBadgeType } from './interfaces/badge.interface'
 
 @Component({
-  selector: 'mib-badge',
-  templateUrl: './badge.component.html',
-  styleUrls: ['./badge.component.scss']
+	selector: 'mib-badge',
+	templateUrl: './badge.component.html',
+	styleUrls: ['./badge.component.scss']
 })
 export class BadgeComponent {
-
+	@Input() size: MibBadgeSize = 'l'
+	@Input() type: MibBadgeType = 'filled-primary'
+	@Input() extended: boolean = false
 }
--- src/app/shared/ui-kit/badge/interfaces/badge.interface.ts
+++ src/app/shared/ui-kit/badge/interfaces/badge.interface.ts
@@ -1,2 +1,6 @@
-export interface BadgeInterface {
-}
+export type MibBadgeSize = 'l' | 'm' | 's'
+export type MibBadgeType =
+	| 'filled-primary'
+	| 'filled-secondary'
+	| 'filled-accent'
+	| 'filled-success'
