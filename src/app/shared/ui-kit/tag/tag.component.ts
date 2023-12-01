bf4cc8416d6e8427e9079705cbab3d58687d55ff
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-12-01 06:10:03 +0600

    init: tag comp, fix checkbox

---
+++ src/app/shared/ui-kit/tag/tag.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mib-tag',
+  templateUrl: './tag.component.html',
+  styleUrls: ['./tag.component.scss']
+})
+export class TagComponent {
+
+}
