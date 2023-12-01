b6a0bbe2cee157ba197e4358fb6982118423377f
Artyom-vv <76263393+Artyom-vv@users.noreply.github.com>
2023-12-01 20:41:33 +0300

    update

--- src/app/shared/ui-kit/input/input.component.ts
+++ src/app/shared/ui-kit/input/input.component.ts
@@ -1,168 +1,166 @@
 import {
-	AfterContentInit,
-	AfterViewChecked,
-	AfterViewInit,
-	Component,
-	ContentChild,
-	ElementRef,
-	Renderer2,
-	ViewChild,
-	ViewEncapsulation
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ContentChild,
+  ElementRef,
+  Renderer2,
+  ViewChild,
+  ViewEncapsulation
 } from '@angular/core'
-import { MibInputDirective } from './directives/mib-input.directive'
-import { fromEvent, tap } from 'rxjs'
+import {MibInputDirective} from './directives/mib-input.directive'
+import {fromEvent, tap} from 'rxjs'
 
 @Component({
-	selector: 'mib-input',
-	templateUrl: './input.component.html',
-	styleUrls: ['./input.component.scss'],
-	encapsulation: ViewEncapsulation.None
+  selector: 'mib-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class InputComponent
-	implements AfterContentInit, AfterViewInit, AfterViewChecked
-{
-	@ContentChild(MibInputDirective, { descendants: true })
-	inputDirective!: MibInputDirective
+export class InputComponent implements AfterContentInit, AfterViewInit, AfterViewChecked {
+  @ContentChild(MibInputDirective, {descendants: true})
+  inputDirective!: MibInputDirective
 
-	@ViewChild('label') labelEl: ElementRef<HTMLSpanElement>
-	@ViewChild('iconsLeft') iconsLeftEl: ElementRef<HTMLDivElement>
-	@ViewChild('iconsRight') iconsRightEl: ElementRef<HTMLDivElement>
-	@ViewChild('box') box: ElementRef<HTMLDivElement>
-	@ViewChild('message') messageRef?: ElementRef
+  @ViewChild('label') labelEl: ElementRef<HTMLSpanElement>
+  @ViewChild('iconsLeft') iconsLeftEl: ElementRef<HTMLDivElement>
+  @ViewChild('iconsRight') iconsRightEl: ElementRef<HTMLDivElement>
+  @ViewChild('box') box: ElementRef<HTMLDivElement>
+  @ViewChild('message') messageRef?: ElementRef
 
-	constructor(private r2: Renderer2) {}
+  public viewMounted: boolean = false;
 
-	ngAfterViewInit(): void {
-		this.setIconPaddings()
+  isResizing: boolean = false
+  startPosX: number = 0
+  startPosY: number = 0
+
+  public classes: string[] = []
+  public input?: HTMLInputElement
+  public textarea?: HTMLTextAreaElement
+
+  public maxLength: number = 0
+  public textLength: number = 0
+
+  constructor(private r2: Renderer2) {
+  }
+
+  ngAfterViewInit(): void {
+    this.setIconPaddings()
     this.updateClasses()
-		// console.log(
-		// 	'inputDirectiveRefNative :>> ',
-		// 	this.inputDirective.elementRef.nativeElement
-		// )
-	}
+    setTimeout(() => this.viewMounted = true)
+  }
 
-	setIconPaddings() {
-		const leftWidth = this.iconsLeftEl.nativeElement.clientWidth
-		const rightWidth = this.iconsRightEl.nativeElement.clientWidth
+  ngAfterContentInit() {
+    if (this.inputDirective) {
+      if (this.inputDirective.elementRef.nativeElement.tagName === 'TEXTAREA') {
+        this.textarea = this.inputDirective.elementRef
+          .nativeElement as unknown as HTMLTextAreaElement
+        this.maxLength = this.textarea.maxLength
+        this.textLength = this.textarea.textLength
+        fromEvent(this.textarea, 'input')
+          .pipe(
+            tap(event => {
+              setTimeout(() => {
+                const target = event.target as HTMLTextAreaElement
+                this.maxLength = target.maxLength
+                this.textLength = target.textLength
+              })
+            })
+          )
+          .subscribe()
+      } else {
+        this.input = this.inputDirective.elementRef
+          .nativeElement as HTMLInputElement
+      }
+    } else {
+      throw new Error('mib-input should contains <input mibInput/>!')
+    }
+  }
 
-		const inputStyles = window.getComputedStyle(
-			this.inputDirective.elementRef.nativeElement
-		)
+  ngAfterViewChecked() {
+    this.updateClasses()
+  }
 
-		const paddingRight = inputStyles.getPropertyValue('padding-right')
-		const paddingLeft = inputStyles.getPropertyValue('padding-left')
+  setIconPaddings() {
+    const leftWidth = this.iconsLeftEl.nativeElement.clientWidth
+    const rightWidth = this.iconsRightEl.nativeElement.clientWidth
 
-		const newPaddingRight = `calc(${paddingRight} + ${rightWidth}px)`
-		const newPaddingLeft = `calc(${paddingLeft} + ${leftWidth}px)`
+    const inputStyles = window.getComputedStyle(
+      this.inputDirective.elementRef.nativeElement
+    )
 
-		this.r2.setStyle(
-			this.inputDirective.elementRef.nativeElement,
-			'padding-right',
-			newPaddingRight
-		)
+    const paddingRight = inputStyles.getPropertyValue('padding-right')
+    const paddingLeft = inputStyles.getPropertyValue('padding-left')
 
-		this.r2.setStyle(
-			this.inputDirective.elementRef.nativeElement,
-			'padding-left',
-			newPaddingLeft
-		)
+    const newPaddingRight = `calc(${paddingRight} + ${rightWidth}px)`
+    const newPaddingLeft = `calc(${paddingLeft} + ${leftWidth}px)`
 
-		this.r2.setStyle(this.labelEl.nativeElement, 'padding-left', newPaddingLeft)
-	}
+    this.r2.setStyle(
+      this.inputDirective.elementRef.nativeElement,
+      'padding-right',
+      newPaddingRight
+    )
 
-	focus() {
-		this.inputDirective.elementRef.nativeElement.focus()
-	}
+    this.r2.setStyle(
+      this.inputDirective.elementRef.nativeElement,
+      'padding-left',
+      newPaddingLeft
+    )
 
-	get isMessageEmpty(): boolean {
-		const node = this.messageRef?.nativeElement
-		for (let i = 0; i < node?.childNodes?.length; i++) {
-			const child = node.childNodes[i]
-			if (
-				child.nodeType !== Node.COMMENT_NODE &&
-				child.textContent.trim() !== ''
-			) {
-				return false
-			}
-		}
-		return true
-	}
+    this.r2.setStyle(this.labelEl.nativeElement, 'padding-left', newPaddingLeft)
+  }
 
-	isResizing: boolean = false
-	startPosX: number = 0
-	startPosY: number = 0
+  focus() {
+    this.inputDirective.elementRef.nativeElement.focus()
+  }
 
-	public classes: string[] = []
-	public input?: HTMLInputElement
-	public textarea?: HTMLTextAreaElement
+  get isMessageEmpty(): boolean {
+    const node = this.messageRef?.nativeElement
+    for (let i = 0; i < node?.childNodes?.length; i++) {
+      const child = node.childNodes[i]
+      if (
+        child.nodeType !== Node.COMMENT_NODE &&
+        child.textContent.trim() !== ''
+      ) {
+        return false
+      }
+    }
+    return true
+  }
 
-	public maxLength: number = 0
-	public textLength: number = 0
+  private updateClasses() {
+    this.classes = []
+    if (this.iconsLeftEl.nativeElement.children.length > 0)
+      this.classes.push('input_left-iconly')
+    if (this.iconsRightEl.nativeElement.children.length > 0)
+      this.classes.push('input_right-iconly')
+    // ERRORIUS!!!!!
+    // this.classes.forEach(clazz => this.input?.classList.add(clazz) || this.textarea?.classList.add(clazz));
+    this.classes.forEach(clazz => this.input?.classList.add(clazz))
+    this.classes.forEach(clazz => this.textarea?.classList.add(clazz))
+  }
 
-	ngAfterContentInit() {
-		if (this.inputDirective) {
-			if (this.inputDirective.elementRef.nativeElement.tagName === 'TEXTAREA') {
-				this.textarea = this.inputDirective.elementRef
-					.nativeElement as unknown as HTMLTextAreaElement
-				this.maxLength = this.textarea.maxLength
-				this.textLength = this.textarea.textLength
-				fromEvent(this.textarea, 'input')
-					.pipe(
-						tap(event => {
-							setTimeout(() => {
-								const target = event.target as HTMLTextAreaElement
-								this.maxLength = target.maxLength
-								this.textLength = target.textLength
-							})
-						})
-					)
-					.subscribe()
-			} else {
-				this.input = this.inputDirective.elementRef
-					.nativeElement as HTMLInputElement
-			}
-		} else {
-			throw new Error('mib-input should contains <input mibInput/>!')
-		}
-	}
+  startResize(event: MouseEvent) {
+    event.preventDefault()
+    this.isResizing = true
+    this.startPosX = event.clientX
+    this.startPosY = event.clientY
+  }
 
-	ngAfterViewChecked() {
-		this.updateClasses()
-	}
+  stopResize($event: MouseEvent) {
+    this.isResizing = false
+  }
 
-	private updateClasses() {
-		this.classes = []
-		if (this.iconsLeftEl.nativeElement.children.length > 0)
-			this.classes.push('input_left-iconly')
-		if (this.iconsRightEl.nativeElement.children.length > 0)
-			this.classes.push('input_right-iconly')
-		// ERRORIUS!!!!!
-		// this.classes.forEach(clazz => this.input?.classList.add(clazz) || this.textarea?.classList.add(clazz));
-		this.classes.forEach(clazz => this.input?.classList.add(clazz))
-		this.classes.forEach(clazz => this.textarea?.classList.add(clazz))
-	}
-
-	startResize(event: MouseEvent) {
-		event.preventDefault()
-		this.isResizing = true
-		this.startPosX = event.clientX
-		this.startPosY = event.clientY
-	}
-
-	stopResize($event: MouseEvent) {
-		this.isResizing = false
-	}
-
-	onMouseMove(event: MouseEvent) {
-		if (this.isResizing) {
-			const deltaX = event.clientX - this.startPosX
-			const deltaY = event.clientY - this.startPosY
-			if (this.textarea) {
-				this.textarea.style.width = this.textarea.offsetWidth + deltaX + 'px'
-				this.textarea.style.height = this.textarea.offsetHeight + deltaY + 'px'
-			}
-			this.startPosX = event.clientX
-			this.startPosY = event.clientY
-		}
-	}
+  onMouseMove(event: MouseEvent) {
+    if (this.isResizing) {
+      const deltaX = event.clientX - this.startPosX
+      const deltaY = event.clientY - this.startPosY
+      if (this.textarea) {
+        this.textarea.style.width = this.textarea.offsetWidth + deltaX + 'px'
+        this.textarea.style.height = this.textarea.offsetHeight + deltaY + 'px'
+      }
+      this.startPosX = event.clientX
+      this.startPosY = event.clientY
+    }
+  }
 }
