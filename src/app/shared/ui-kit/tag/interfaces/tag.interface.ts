cec29633e5811218fc1361c398e25fe1da78f803
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-12-01 06:55:23 +0600

    feat: add basic tag comp structure

--- src/app/shared/ui-kit/tag/interfaces/tag.interface.ts
+++ src/app/shared/ui-kit/tag/interfaces/tag.interface.ts
@@ -1,2 +1,8 @@
-export interface TagInterface {
-}
+export type TagSize = 'l' | 'm' | 's'
+export type TagStatus = 'default' | 'hover' | 'active' | 'disabled'
+export type TagType =
+	| 'filled-primary'
+	| 'filled-secondary'
+	| 'floating'
+	| 'filled-accent'
+	| 'outline'
--- src/app/shared/ui-kit/tag/tag.component.ts
+++ src/app/shared/ui-kit/tag/tag.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { TagSize, TagStatus, TagType } from './interfaces/tag.interface'
 
 @Component({
-  selector: 'mib-tag',
-  templateUrl: './tag.component.html',
-  styleUrls: ['./tag.component.scss']
+	selector: 'mib-tag',
+	templateUrl: './tag.component.html',
+	styleUrls: ['./tag.component.scss']
 })
 export class TagComponent {
+	@Input() value?: any
+	@Input() size: TagSize = 'l'
+	@Input() status: TagStatus = 'default'
+	@Input() flex: string = 'flex_center'
+	@Input() type: TagType = 'filled-primary'
+	@Input() disabled: boolean = false
+	@Output() onClick: EventEmitter<any> = new EventEmitter<any>()
+	@Output() press: EventEmitter<any> = new EventEmitter<any>()
 
+	ngAfterViewInit() {
+		if (this.disabled) this.status = 'disabled'
+	}
 }
