29b8ec8bb38156ca5d34241903bb1b86634d03b6
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-12-06 17:52:34 +0600

    feat: add request-page w empty drawer

--- src/app/client/modules/requests/modules/request-drawer/request-drawer.component.ts
+++ src/app/client/modules/requests/modules/request-drawer/request-drawer.component.ts
@@ -1,53 +1,44 @@
-// import { Component, Inject, OnInit } from '@angular/core'
-// import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'
-// import { DrawerData } from '../../../../../shared/ui-kit/drawer/interfaces/drawer.interface'
-// import { AdvancedRequests } from '../../components/requests-page/interfaces/requests-page.interface'
-// import { ToolsService } from '../../../../../shared/services/tools.service'
-// import { RequestDrawerService } from './request-drawer.service'
-// import { BehaviorSubject, finalize, tap } from 'rxjs'
-// import { RequestsResponse } from '../../../requests/types/requestResponse.interface'
-
-// @Component({
-// 	selector: 'mib-request-drawer',
-// 	templateUrl: './request-drawer.component.html',
-// 	styleUrls: ['./request-drawer.component.scss']
-// })
-// export class RequestDrawerComponent implements OnInit {
-// 	public sending$ = new BehaviorSubject<boolean>(false)
-
-// 	public PAGINATOR_ITEMS_PER_PAGE = 6
-// 	public PAGINATOR_PAGE_TO_SHOW = 5
-
-// 	public dutiesVisible: AdvancedRequests[] = []
-
-// 	constructor(
-// 		public dialogRef: MatDialogRef<RequestDrawerComponent>,
-// 		public toolsService: ToolsService,
-// 		private requestsService: RequestDrawerService,
-// 		@Inject(MAT_DIALOG_DATA) public data: DrawerData<AdvancedRequests[]>
-// 	) {}
-
-// 	trackByFunction(index, data: RequestsResponse) {
-// 		return data.ID
-// 	}
-
-// 	ngOnInit() {
-// 		this.onPageChange(1)
-// 	}
-
-// 	onPageChange(page: number) {
-// 		const startIndex = (page - 1) * this.PAGINATOR_ITEMS_PER_PAGE
-// 		const endIndex = startIndex + this.PAGINATOR_ITEMS_PER_PAGE
-
-// 		this.dutiesVisible = this.data.data.slice(startIndex, endIndex)
-// 	}
-// }
-
-import { Component } from '@angular/core'
+import { Component, Inject, OnInit } from '@angular/core'
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'
+import { DrawerData } from '../../../../../shared/ui-kit/drawer/interfaces/drawer.interface'
+import { AdvancedRequests } from '../../pages/requests-page/interfaces/requests-page.interface'
+import { ToolsService } from '../../../../../shared/services/tools.service'
+import { RequestDrawerService } from './request-drawer.service'
+import { BehaviorSubject, finalize, tap } from 'rxjs'
+import { RequestsResponse } from '../../../requests/types/requestResponse.interface'
 
 @Component({
 	selector: 'mib-request-drawer',
 	templateUrl: './request-drawer.component.html',
 	styleUrls: ['./request-drawer.component.scss']
 })
-export class RequestDrawerComponent {}
+export class RequestDrawerComponent implements OnInit {
+	public sending$ = new BehaviorSubject<boolean>(false)
+
+	public PAGINATOR_ITEMS_PER_PAGE = 1
+	public PAGINATOR_PAGE_TO_SHOW = 1
+
+	public dutiesVisible: AdvancedRequests[] = []
+
+	constructor(
+		public dialogRef: MatDialogRef<RequestDrawerComponent>,
+		public toolsService: ToolsService,
+		private requestsService: RequestDrawerService,
+		@Inject(MAT_DIALOG_DATA) public data: DrawerData<AdvancedRequests[]>
+	) {}
+
+	trackByFunction(index, data: RequestsResponse) {
+		return data.ID
+	}
+
+	ngOnInit() {
+		this.onPageChange(1)
+	}
+
+	onPageChange(page: number) {
+		const startIndex = (page - 1) * this.PAGINATOR_ITEMS_PER_PAGE
+		const endIndex = startIndex + this.PAGINATOR_ITEMS_PER_PAGE
+
+		this.dutiesVisible = this.data.data.slice(startIndex, endIndex)
+	}
+}
