265114094f2ea35d4443da0abc5aa0b2f5433c1a
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-12-07 07:26:52 +0600

    init: header comp

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,59 +1,58 @@
-import {ConfirmPasswordPageComponent} from './components/confirm-password-page/confirm-password-page.component';
-import {ResetPasswordPageComponent} from './components/reset-password-page/reset-password-page.component';
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { ConfirmPasswordPageComponent } from './components/confirm-password-page/confirm-password-page.component'
+import { ResetPasswordPageComponent } from './components/reset-password-page/reset-password-page.component'
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
-import {CardModule} from 'primeng/card';
-import {ButtonModule} from 'primeng/button';
-import {MessagesModule} from 'primeng/messages';
-import {MessageModule} from 'primeng/message';
-import {InputMaskModule} from 'primeng/inputmask';
-import {InputTextModule} from 'primeng/inputtext';
-import {DropdownModule} from 'primeng/dropdown';
-import {ProgressSpinnerModule} from 'primeng/progressspinner';
+import { CardModule } from 'primeng/card'
+import { ButtonModule } from 'primeng/button'
+import { MessagesModule } from 'primeng/messages'
+import { MessageModule } from 'primeng/message'
+import { InputMaskModule } from 'primeng/inputmask'
+import { InputTextModule } from 'primeng/inputtext'
+import { DropdownModule } from 'primeng/dropdown'
+import { ProgressSpinnerModule } from 'primeng/progressspinner'
 
-import {CommonService} from '../shared/services/common/common.service';
-import {RegisterPageComponent} from './components/register-page/register-page.component';
-import {LoginPageComponent} from './components/login-page/login-page.component';
-import {AuthRoutingModule} from './auth-routing.module';
-import {AuthComponent} from './auth.component';
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
-import {AuthInterceptor} from './services/auth.interceptor';
-import {TooltipModule} from 'primeng/tooltip';
+import { CommonService } from '../shared/services/common/common.service'
+import { RegisterPageComponent } from './components/register-page/register-page.component'
+import { LoginPageComponent } from './components/login-page/login-page.component'
+import { AuthRoutingModule } from './auth-routing.module'
+import { AuthComponent } from './auth.component'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { AuthInterceptor } from './services/auth.interceptor'
+import { TooltipModule } from 'primeng/tooltip'
 
 @NgModule({
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    InputTextModule,
-    DropdownModule,
-    ProgressSpinnerModule,
-    CardModule,
-    MessageModule,
-    MessagesModule,
-    ButtonModule,
-    InputMaskModule,
-    TooltipModule,
-  ],
-  declarations: [
-    AuthComponent,
-    LoginPageComponent,
-    RegisterPageComponent,
-    ResetPasswordPageComponent,
-    ConfirmPasswordPageComponent,
-  ],
-  providers: [
-    CommonService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true,
-    },
-  ],
+	imports: [
+		CommonModule,
+		AuthRoutingModule,
+		FormsModule,
+		HttpClientModule,
+		ReactiveFormsModule,
+		InputTextModule,
+		DropdownModule,
+		ProgressSpinnerModule,
+		CardModule,
+		MessageModule,
+		MessagesModule,
+		ButtonModule,
+		InputMaskModule,
+		TooltipModule
+	],
+	declarations: [
+		AuthComponent,
+		LoginPageComponent,
+		RegisterPageComponent,
+		ResetPasswordPageComponent,
+		ConfirmPasswordPageComponent
+	],
+	providers: [
+		CommonService,
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: AuthInterceptor,
+			multi: true
+		}
+	]
 })
-export class AuthModule {
-}
+export class AuthModule {}
