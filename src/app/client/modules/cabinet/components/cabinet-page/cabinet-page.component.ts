374aa67988e8982dedc1241acf87db897c89d24e
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-12-09 18:25:29 +0600

    feat: add dynamic data cach.comp

--- src/app/client/modules/cabinet/components/cabinet-page/cabinet-page.component.ts
+++ src/app/client/modules/cabinet/components/cabinet-page/cabinet-page.component.ts
@@ -1,35 +1,39 @@
-import { StatisticsInterface } from '../../types/common/statistics.interface';
-import { ReportCardInterface } from '../../types/common/report-card.interface';
-import { AuthService } from '../../../../../auth/services/auth.service';
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { MIBCommon } from 'src/app/shared/classes/common/mid-common.class';
-import { StatisticsService } from '../../services/statistics.service';
+import { StatisticsInterface } from '../../types/common/statistics.interface'
+import { ReportCardInterface } from '../../types/common/report-card.interface'
+import { AuthService } from '../../../../../auth/services/auth.service'
+import { Component, OnInit, ViewEncapsulation } from '@angular/core'
+import { MIBCommon } from 'src/app/shared/classes/common/mid-common.class'
+import { StatisticsService } from '../../services/statistics.service'
 
 @Component({
-  selector: 'app-cabinet-page',
-  templateUrl: './cabinet-page.component.html',
-  styleUrls: ['./cabinet-page.component.scss'],
-  encapsulation: ViewEncapsulation.None
+	selector: 'app-cabinet-page',
+	templateUrl: './cabinet-page.component.html',
+	styleUrls: ['./cabinet-page.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class CabinetPageComponent implements OnInit {
-  public statistics: StatisticsInterface;
-  public reportCards: ReportCardInterface[] = [];
+	public statistics: StatisticsInterface
+	public reportCards: ReportCardInterface[] = []
 
-  constructor(private authSerice: AuthService, private statisticsService: StatisticsService) {}
+	constructor(
+		private authSerice: AuthService,
+		private statisticsService: StatisticsService
+	) {}
 
-  ngOnInit() {
-    this.reportCardsInit();
-    this.statisticsService.getClientStatistics().subscribe(resp => {
-      this.statistics = resp;
-    })
-  }
+	ngOnInit() {
+		this.reportCardsInit()
+		this.statisticsService.getClientStatistics().subscribe(resp => {
+			this.statistics = resp
+			console.log('statistics :>> ', this.statistics)
+		})
+	}
 
-  reportCardsInit() {
-    let mibCommon = new MIBCommon();
-    this.reportCards = mibCommon.getReports();
-  }
+	reportCardsInit() {
+		let mibCommon = new MIBCommon()
+		this.reportCards = mibCommon.getReports()
+	}
 
-  public logout(): void {
-    this.authSerice.logout();
-  }
+	public logout(): void {
+		this.authSerice.logout()
+	}
 }
--- src/app/shared/modules/cash-panel/cash-panel.component.ts
+++ src/app/shared/modules/cash-panel/cash-panel.component.ts
@@ -1,29 +1,33 @@
-import {AfterViewInit, ChangeDetectorRef, Component, Input} from '@angular/core';
-import {DeviceType} from '../../interfaces/shared.interface';
-import {OpacityViewAnimation} from '../../animations/animations';
+import {
+	AfterViewInit,
+	ChangeDetectorRef,
+	Component,
+	Input
+} from '@angular/core'
+import { DeviceType } from '../../interfaces/shared.interface'
+import { OpacityViewAnimation } from '../../animations/animations'
 
 @Component({
-  selector: 'mib-cash-panel',
-  templateUrl: './cash-panel.component.html',
-  styleUrls: ['./cash-panel.component.scss'],
-  animations: [
-    OpacityViewAnimation
-  ]
+	selector: 'mib-cash-panel',
+	templateUrl: './cash-panel.component.html',
+	styleUrls: ['./cash-panel.component.scss'],
+	animations: [OpacityViewAnimation]
 })
 export class CashPanelComponent implements AfterViewInit {
+	@Input() device: DeviceType = 'desktop'
+	@Input() viewLimit: number
+	@Input() relevantAt: string
 
-  @Input() device: DeviceType = 'desktop'
+	public isHover: boolean = false
+	public viewMounted: boolean = false
 
-  public isHover: boolean = false;
-  public viewMounted: boolean = false;
+	get classes() {
+		return {
+			[`cash-panel_${this.device}`]: true
+		}
+	}
 
-  get classes() {
-    return {
-      [`cash-panel_${this.device}`]: true,
-    }
-  }
-
-  ngAfterViewInit() {
-    this.viewMounted = true;
-  }
+	ngAfterViewInit() {
+		this.viewMounted = true
+	}
 }
