a428c05a3cbd1add0300700e768409b81140118c
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-12-11 18:08:24 +0600

    init: new-demand.comp

---
+++ src/app/shared/modules/new-demand/new-demand.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mib-new-demand',
+  templateUrl: './new-demand.component.html',
+  styleUrls: ['./new-demand.component.scss']
+})
+export class NewDemandComponent {
+
+}
