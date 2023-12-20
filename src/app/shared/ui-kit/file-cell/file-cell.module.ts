f2838e9a1ec7268575c6f55f677e40b1cf59b23b
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-12-20 19:36:57 +0600

    add: serv/int-face, fix lgc file-cell.comp

--- src/app/shared/ui-kit/file-cell/file-cell.module.ts
+++ src/app/shared/ui-kit/file-cell/file-cell.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FileCellComponent } from './file-cell.component'
 import { RefIconModule } from '../ref-icon/ref-icon.module'
+import { FileService } from '../../services/common/file.service'
 
 @NgModule({
 	declarations: [FileCellComponent],
 	exports: [FileCellComponent],
-	imports: [CommonModule, RefIconModule]
+	imports: [CommonModule, RefIconModule],
+	providers: [FileService]
 })
 export class FileCellModule {}
