5adf99838bff555a60795820346019d465dbc46a
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-12-20 19:58:08 +0600

    init: req.bro.drawer.comp

---
+++ src/app/client/modules/requests/modules/request-browser-drawer/request-browser-drawer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mib-request-browser-drawer',
+  templateUrl: './request-browser-drawer.component.html',
+  styleUrls: ['./request-browser-drawer.component.scss']
+})
+export class RequestBrowserDrawerComponent {
+
+}
