6a31e229e86ea1c5175ec4bda73ff7024d4521af
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-01-10 15:31:17 +0600

    refactor: moved req-cr-suc-modal, del f-banners

--- src/app/shared/ui-kit/modal/components/banner-success/banner-success.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core'
-
-@Component({
-	selector: 'mib-banner-success',
-	templateUrl: './banner-success.component.html',
-	styleUrls: ['./banner-success.component.scss']
-})
-export class BannerSuccessComponent {}
--- src/app/shared/ui-kit/modal/modal.module.ts
+++ src/app/shared/ui-kit/modal/modal.module.ts
@@ -6,9 +6,6 @@ import {ModalHeaderComponent} from './components/modal-header/modal-header.compo
 import {ButtonModule} from '../button/button.module'
 import {SpacingModule} from '../spacing/spacing.module'
 import {IconModule} from '../ref-icon/icon.module'
-import {BannerSuccessComponent} from './components/banner-success/banner-success.component'
-import {BannerFailureComponent} from './components/banner-failure/banner-failure.component'
-import {BannerInfoComponent} from './components/banner-info/banner-info.component'
 import {BannerModalComponent} from './components/banner-modal/banner-modal.component'
 
 @NgModule({
@@ -16,9 +13,6 @@ import {BannerModalComponent} from './components/banner-modal/banner-modal.compo
 		ModalComponent,
 		ModalFooterComponent,
 		ModalHeaderComponent,
-		BannerSuccessComponent,
-		BannerFailureComponent,
-		BannerInfoComponent,
 		BannerModalComponent
 	],
 	imports: [CommonModule, ButtonModule, IconModule, SpacingModule],
@@ -26,9 +20,6 @@ import {BannerModalComponent} from './components/banner-modal/banner-modal.compo
 		ModalComponent,
 		ModalFooterComponent,
 		ModalHeaderComponent,
-		BannerSuccessComponent,
-		BannerFailureComponent,
-		BannerInfoComponent,
 		BannerModalComponent
 	]
 })
