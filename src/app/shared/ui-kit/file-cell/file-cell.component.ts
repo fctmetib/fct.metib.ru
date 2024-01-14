5a47aafeaf751bb9a410b7b475434de403d1f390
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-01-14 20:21:02 +0600

    add: file-cell.comp basic view

--- src/app/shared/ui-kit/file-cell/file-cell.component.ts
+++ src/app/shared/ui-kit/file-cell/file-cell.component.ts
@@ -61,21 +61,17 @@ export class FileCellComponent {
 		}
 	}
 
-	//----------------------------------------------------
 	onUpload() {
 		if (this.file) {
-			// const upload$ = this.filesService
 			this.uploadSub = this.filesService
 				.uploadFileWithProgress(this.file)
 				.subscribe({
 					next: event => {
 						switch (event.type) {
 							case HttpEventType.UploadProgress:
-								console.log('HttpEventType :>> ', HttpEventType)
 								this.status = 'uploading'
 								if (event.total) {
 									this.progress = Math.round((100 * event.loaded) / event.total)
-									console.log('progress :>> ', this.progress)
 								}
 								break
 							case HttpEventType.Response:
@@ -93,32 +89,6 @@ export class FileCellComponent {
 		}
 	}
 
-	//----------------------------------------------------
-
-	// onUpload() {
-	// 	if (this.file) {
-	// 		const formData = new FormData()
-	// 		formData.append('file', this.file, this.file.name)
-
-	// 		const upload$ = this.http.post('https://httpbin.org/post', formData, {
-	// 			// reportProgress: true,
-	// 			// observe: 'events'
-	// 		})
-
-	// 		this.status = 'uploading'
-
-	// 		this.uploadSub = upload$.subscribe({
-	// 			next: () => {
-	// 				this.status = 'success'
-	// 			},
-	// 			error: (error: any) => {
-	// 				this.status = 'fail'
-	// 				return throwError(() => error)
-	// 			}
-	// 		})
-	// 	}
-	// }
-
 	cancelUpload() {
 		if (!(<Subscription>this.uploadSub)) {
 			return
@@ -134,145 +104,3 @@ export class FileCellComponent {
 		this.file = null
 	}
 }
-
-//------------------------------------------------
-// get classes() {
-// 	return {
-// 		[`file-cell_status-${this.status}`]: true,
-// 		[`file-cell_content-${this.content}`]: true,
-// 		[`file-cell_size-${this.size}`]: true
-// 	}
-// }
-
-// get IconName() {
-// 	let icon = ''
-// 	switch (this.content) {
-// 		case 'document':
-// 			icon = 'fi_file'
-// 			break
-// 	}
-// 	return icon
-// }
-
-// ngOnInit(): void {}
-
-// onChange(event: any) {
-// 	const file: File = event.target.files[0]
-
-// 	if (file) {
-// 		this.status = 'initial'
-// 		this.file = file
-// 		this.fileName = file.name
-// 		const ft = this.file.type
-// 		if (ft === 'image/png' || ft === 'image/jpg' || ft === 'image/jpeg') {
-// 			this.content = 'image'
-// 		} else {
-// 			this.content = 'document'
-// 		}
-// 	}
-// }
-
-// onUpload() {
-// 	if (this.file) {
-// 		const formData = new FormData()
-// 		formData.append('file', this.file, this.file.name)
-
-// 		const upload$ = this.http.post('https://httpbin.org/post', formData, {
-// 			// reportProgress: true,
-// 			// observe: 'events'
-// 		})
-
-// 		this.status = 'uploading'
-
-// 		this.uploadSub = upload$.subscribe({
-// 			next: () => {
-// 				this.status = 'success'
-// 			},
-// 			error: (error: any) => {
-// 				this.status = 'fail'
-// 				return throwError(() => error)
-// 			}
-// 		})
-// 	}
-// }
-
-// cancelUpload() {
-// 	if (!(<Subscription>this.uploadSub)) {
-// 		return
-// 	} else {
-// 		this.uploadSub.unsubscribe()
-// 	}
-// 	this.reset()
-// }
-
-// reset() {
-// 	this.status = ''
-// 	this.fileName = ''
-// 	this.file = null
-// }
-// }
-//------------------------------------------------
-
-// 	ngOnInit(): void {}
-
-// 	onChange(event: any) {
-// 		const file: File = event.target.files[0]
-
-// 		if (file) {
-// 			this.status = 'initial'
-// 			this.file = file
-// 			this.fileName = file.name
-// 			const ft = this.file.type
-// 			if (ft === 'image/png' || ft === 'image/jpg' || ft === 'image/jpeg') {
-// 				this.content = 'image'
-// 			} else {
-// 				this.content = 'document'
-// 			}
-// 		}
-// 	}
-
-// 	onUpload() {
-// 		if (this.file) {
-// 			const formData = new FormData()
-// 			formData.append('file', this.file, this.file.name)
-
-// 			const upload$ = this.http.post('https://httpbin.org/post', formData, {
-// 				reportProgress: true,
-// 				observe: 'events'
-// 			})
-// 			// .pipe(finalize(() => this.reset()))
-
-// 			this.status = 'uploading'
-// 			// -----------
-
-// 			// upload$.subscribe(event => {
-// 			// 	console.log('event :>> ', event)
-// 			// 	if (event.type == HttpEventType.UploadProgress) {
-// 			// 		this.uploadProgress = Math.round(100 * (event.loaded / event.total))
-// 			// 	}
-// 			// })
-// 			// .unsubscribe()
-
-// 			upload$.subscribe({
-// 				next: () => {
-// 					this.status = 'success'
-// 				},
-// 				error: (error: any) => {
-// 					this.status = 'fail'
-// 					return throwError(() => error)
-// 				}
-// 			})
-// 		}
-// 	}
-
-// 	cancelUpload() {
-// 		// this.uploadSub.unsubscribe()
-// 		this.reset()
-// 	}
-
-// 	reset() {
-// 		this.file = null
-// 		this.status = ''
-// 		this.fileName = ''
-// 	}
-// }
--- src/app/shared/ui-kit/file-cell/file-cell.module.ts
+++ src/app/shared/ui-kit/file-cell/file-cell.module.ts
@@ -4,11 +4,12 @@ import {FileCellComponent} from './file-cell.component'
 import {IconModule} from '../ref-icon/icon.module'
 import {FileService} from '../../services/common/file.service'
 import {AvatarModule} from '../avatar/avatar.module'
+import {ButtonModule} from '../button/button.module'
 
 @NgModule({
 	declarations: [FileCellComponent],
 	exports: [FileCellComponent],
-	imports: [CommonModule, IconModule, AvatarModule],
+	imports: [CommonModule, IconModule, AvatarModule, ButtonModule],
 	providers: [FileService]
 })
 export class FileCellModule {}
