b17b238fcefdd87553ddbf8335a588005e56c805
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-01-20 08:02:08 +0600

    add: dynamic data w pagination

--- src/app/client/modules/contracts/pages/new-contracts-page/new-contracts-page.component.ts
+++ src/app/client/modules/contracts/pages/new-contracts-page/new-contracts-page.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, OnInit, ViewChild} from '@angular/core'
 import {Properties} from 'csstype'
-import {BehaviorSubject} from 'rxjs'
+import {BehaviorSubject, finalize, tap} from 'rxjs'
 import {DeliveryService} from 'src/app/shared/services/share/delivery.service'
 import {ToolsService} from 'src/app/shared/services/tools.service'
+import {DeliveryContractsInterface} from 'src/app/shared/types/delivery/delivery-contracts.interface'
+import {TableComponent} from 'src/app/shared/ui-kit/table/table.component'
 
 @Component({
 	selector: 'mib-new-contracts-page',
@@ -11,6 +13,8 @@ import {ToolsService} from 'src/app/shared/services/tools.service'
 })
 export class NewContractsPageComponent implements OnInit {
 	public loading$ = new BehaviorSubject<boolean>(false)
+
+	@ViewChild(TableComponent) table: TableComponent
 
 	public skeletonWithoutUnderline: Properties = {
 		borderRadius: '8px',
@@ -23,12 +27,15 @@ export class NewContractsPageComponent implements OnInit {
 		borderBottom: '1px solid var(--wgr-tertiary)'
 	}
 
-	public PAGINATOR_ITEMS_PER_PAGE = 10
+	public PAGINATOR_ITEMS_PER_PAGE = 7
 	public PAGINATOR_PAGE_TO_SHOW = 5
 	public currentPage$ = new BehaviorSubject<number>(1)
 
 	public isClosedContracts = false
 	public addStatistics = false
+
+	public listOfContracts: DeliveryContractsInterface[] = []
+	public listOfContractsVisible: DeliveryContractsInterface[] = []
 
 	constructor(
 		public toolsService: ToolsService,
@@ -40,21 +47,36 @@ export class NewContractsPageComponent implements OnInit {
 	}
 
 	getAllDeliveriesContracts() {
+		this.loading$.next(true)
 		this.deliveryService
 			.getAllDeliveriesContracts(this.isClosedContracts, this.addStatistics)
-			.subscribe({
-				next: data => {
-					console.log('data :>> ', data)
-				}
-			})
+			.pipe(
+				tap(data => {
+					this.listOfContracts = data
+					this.onPageChange(this.currentPage$.value)
+					console.log('this.listOfContracts :>> ', this.listOfContracts)
+				}),
+				finalize(() => this.loading$.next(false))
+			)
+			.subscribe()
 	}
 
 	selectionChange($event) {
-		console.log('$event :>> ', $event)
+		// console.log('selectionChange :>> ')
 	}
 
-	onPageChange($event) {
-		console.log('$event :>> ', $event)
+	onPageChange(page: number) {
+		this.currentPage$.next(page)
+
+		const startIndex = (page - 1) * this.PAGINATOR_ITEMS_PER_PAGE
+		const endIndex = startIndex + this.PAGINATOR_ITEMS_PER_PAGE
+
+		this.table.deselect()
+
+		this.listOfContractsVisible = this.listOfContracts.slice(
+			startIndex,
+			endIndex
+		)
 	}
 
 	newContractPageDrawer() {
