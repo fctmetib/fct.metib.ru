bafe88c0170d0a323f43bca7da09fd587f760c9f
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-02-05 19:58:34 +0600

    fix: toaster.comp

--- src/app/shared/ui-kit/toaster/toaster.module.ts
+++ src/app/shared/ui-kit/toaster/toaster.module.ts
@@ -3,10 +3,11 @@ import {CommonModule} from '@angular/common'
 import {ToasterComponent} from './toaster.component'
 import {IconModule} from '../ref-icon/icon.module'
 import {ToasterPointComponent} from './components/toaster-point/toaster-point.component'
+import {ButtonModule} from '../button/button.module'
 
 @NgModule({
 	declarations: [ToasterComponent, ToasterPointComponent],
 	exports: [ToasterComponent, ToasterPointComponent],
-	imports: [CommonModule, IconModule]
+	imports: [CommonModule, IconModule, ButtonModule]
 })
 export class ToasterModule {}
