7190321df2c742058fb9bc2f0c961b273f42a343
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-02-09 17:45:09 +0600

    fix: demand route

--- src/app/client/modules/demand-new/demand-new-routing.module.ts
+++ src/app/client/modules/demand-new/demand-new-routing.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core'
-import { RouterModule } from '@angular/router'
-import { DemandActionComponent } from './containers/demand-action/demand-action.component'
-import { DemandActionsComponent } from './containers/demand-actions/demand-actions.component'
-import { DemandHistoryComponent } from './containers/demand-history/demand-history.component'
-import { DemandNewHomeComponent } from './pages/demand-new-home/demand-new-home.component'
+import {NgModule} from '@angular/core'
+import {RouterModule} from '@angular/router'
+import {DemandActionComponent} from './containers/demand-action/demand-action.component'
+import {DemandActionsComponent} from './containers/demand-actions/demand-actions.component'
+import {DemandHistoryComponent} from './containers/demand-history/demand-history.component'
+import {DemandNewHomeComponent} from './pages/demand-new-home/demand-new-home.component'
 
 const routes = [
 	{
@@ -11,7 +11,7 @@ const routes = [
 		children: [
 			{
 				path: '',
-				component: DemandActionsComponent
+				component: DemandNewHomeComponent
 			},
 			{
 				path: 'demand-action',
@@ -23,7 +23,7 @@ const routes = [
 			},
 			{
 				path: 'home',
-				component: DemandNewHomeComponent
+				component: DemandActionsComponent
 			}
 		]
 	}
