cd9cd93aaef35203f52c7bdb03a4cc7281181d4f
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-02-12 14:53:04 +0600

    feat: demand-signature-drawer basic view

--- src/app/client/modules/demand-new/modules/demand-signature-drawer/demand-signature-drawer.module.ts
+++ src/app/client/modules/demand-new/modules/demand-signature-drawer/demand-signature-drawer.module.ts
@@ -4,10 +4,24 @@ import {DemandSignatureDrawerComponent} from './demand-signature-drawer.componen
 import {DrawerModule} from 'src/app/shared/ui-kit/drawer/drawer.module'
 import {DemandSignatureDrawerService} from './demand-signature-drawer.service'
 import {SpacingModule} from 'src/app/shared/ui-kit/spacing/spacing.module'
+import {InformationModule} from 'src/app/shared/ui-kit/information/information.module'
+import {NavbarModule} from 'src/app/shared/ui-kit/navbar/navbar.module'
+import {ButtonModule} from 'src/app/shared/ui-kit/button/button.module'
+import {InputModule} from 'src/app/shared/ui-kit/input/input.module'
+import {LabelModule} from 'src/app/shared/directives/label/label.module'
 
 @NgModule({
 	declarations: [DemandSignatureDrawerComponent],
-	imports: [CommonModule, DrawerModule, SpacingModule],
+	imports: [
+		CommonModule,
+		DrawerModule,
+		SpacingModule,
+		InformationModule,
+		NavbarModule,
+		ButtonModule,
+		InputModule,
+		LabelModule
+	],
 	providers: [DemandSignatureDrawerService]
 })
 export class DemandSignatureDrawerModule {}
