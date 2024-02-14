87cdc1f6e2c90685ffd8a284ac585db9cdb63eef
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-02-14 16:11:37 +0600

    upd: getInvoice mthd

--- src/app/client/modules/invoices/modules/invoice-drawer/invoice-drawer.component.ts
+++ src/app/client/modules/invoices/modules/invoice-drawer/invoice-drawer.component.ts
@@ -6,115 +6,136 @@ import {DrawerData} from 'src/app/shared/ui-kit/drawer/interfaces/drawer.interfa
 import {InvoiceDrawer} from './interfaces/invoice-drawer.interface'
 import {InvoicesService} from '../../services/invoices.service'
 import {InputSize} from 'src/app/shared/ui-kit/input/interfaces/input.interface'
-import {BankDetailsData} from '../../../../../shared/ui-kit/contracted-forms/interfaces/contracted-forms.interface';
-import {ExtendedClientInvoice, PaymentLink} from '../../interfaces/client.invoice';
-import {ToolsService} from '../../../../../shared/services/tools.service';
+import {BankDetailsData} from '../../../../../shared/ui-kit/contracted-forms/interfaces/contracted-forms.interface'
+import {
+	ExtendedClientInvoice,
+	PaymentLink
+} from '../../interfaces/client.invoice'
+import {ToolsService} from '../../../../../shared/services/tools.service'
 
 @Component({
-  selector: 'mib-invoice-drawer',
-  templateUrl: './invoice-drawer.component.html',
-  styleUrls: ['./invoice-drawer.component.scss']
+	selector: 'mib-invoice-drawer',
+	templateUrl: './invoice-drawer.component.html',
+	styleUrls: ['./invoice-drawer.component.scss']
 })
 export class InvoiceDrawerComponent implements OnInit {
-  public loading$ = new BehaviorSubject<boolean>(false)
+	public loading$ = new BehaviorSubject<boolean>(false)
 
-  public skeletonWithoutUnderline: Properties = {
-    borderRadius: '8px',
-    width: '100%'
-  }
+	public skeletonWithoutUnderline: Properties = {
+		borderRadius: '8px',
+		width: '100%'
+	}
 
-  public skeletonTitle: Properties = {
-    ...this.skeletonWithoutUnderline,
-    height: '28px'
-  }
+	public skeletonTitle: Properties = {
+		...this.skeletonWithoutUnderline,
+		height: '28px'
+	}
 
-  public skeletonDate: Properties = {
-    ...this.skeletonWithoutUnderline,
-    height: '40px'
-  }
+	public skeletonDate: Properties = {
+		...this.skeletonWithoutUnderline,
+		height: '40px'
+	}
 
-  public skeletonTextarea: Properties = {
-    ...this.skeletonWithoutUnderline,
-    height: '64px'
-  }
+	public skeletonTextarea: Properties = {
+		...this.skeletonWithoutUnderline,
+		height: '64px'
+	}
 
-  public skeletonTabGroup: Properties = {
-    ...this.skeletonWithoutUnderline,
-    height: '271px'
-  }
+	public skeletonTabGroup: Properties = {
+		...this.skeletonWithoutUnderline,
+		height: '271px'
+	}
 
-  public skeletonTable: Properties = {
-    ...this.skeletonWithoutUnderline,
-    borderBottom: '1px solid var(--wgr-tertiary)'
-  }
+	public skeletonTable: Properties = {
+		...this.skeletonWithoutUnderline,
+		borderBottom: '1px solid var(--wgr-tertiary)'
+	}
 
-  public PAGINATOR_ITEMS_PER_PAGE = 5
-  public PAGINATOR_PAGE_TO_SHOW = 5
-  public currentPage$ = new BehaviorSubject<number>(1)
+	public PAGINATOR_ITEMS_PER_PAGE = 5
+	public PAGINATOR_PAGE_TO_SHOW = 5
+	public currentPage$ = new BehaviorSubject<number>(1)
 
-  public inputSize: InputSize = 'l'
-  public invoice: ExtendedClientInvoice
+	public inputSize: InputSize = 'l'
+	public invoice: ExtendedClientInvoice
 
-  public invoicesLinks: PaymentLink[] = []
-  public invoicesLinksVisible: PaymentLink[] = []
+	public invoicesLinks: PaymentLink[] = []
+	public invoicesLinksVisible: PaymentLink[] = []
 
-  public totalSumDutyDebtor: number = 0;
+	public totalSumDutyDebtor: number = 0
 
-  constructor(
-    @Inject(MAT_DIALOG_DATA) public data: DrawerData<InvoiceDrawer>,
-    public dialogRef: MatDialogRef<InvoiceDrawerComponent>,
-    private invoicesService: InvoicesService,
-    public toolsService: ToolsService
-  ) {
-  }
+	constructor(
+		@Inject(MAT_DIALOG_DATA) public data: DrawerData<InvoiceDrawer>,
+		public dialogRef: MatDialogRef<InvoiceDrawerComponent>,
+		private invoicesService: InvoicesService,
+		public toolsService: ToolsService
+	) {}
 
-  ngOnInit(): void {
-    this.getCurrentInvoice().subscribe()
-  }
+	ngOnInit(): void {
+		this.getCurrentInvoice().subscribe()
+	}
 
-  get invoiceID() {
-    return this.data.data.invoiceId
-  }
+	get invoiceID() {
+		return this.data.data.invoiceId
+	}
 
-  get beneficiaryRequisites(): BankDetailsData {
-    return {
-      paymentParticipant: this.invoice.Beneficiary,
-      closeDate: this.invoice.Date,
-      openDate: this.invoice.Date,
-    }
-  }
+	get beneficiaryRequisites(): BankDetailsData {
+		return {
+			paymentParticipant: this.invoice.Beneficiary,
+			closeDate: this.invoice.Date,
+			openDate: this.invoice.Date
+		}
+	}
 
-  get payerRequisites(): BankDetailsData {
-    return {
-      paymentParticipant: this.invoice.Payer,
-      closeDate: this.invoice.Date,
-      openDate: this.invoice.Date,
-    }
-  }
+	get payerRequisites(): BankDetailsData {
+		return {
+			paymentParticipant: this.invoice.Payer,
+			closeDate: this.invoice.Date,
+			openDate: this.invoice.Date
+		}
+	}
 
-  getCurrentInvoice() {
-    this.loading$.next(true)
-    return this.invoicesService.getInvoices({
-      includeLinks: true
-    }).pipe(
-      map(invoices => {
-        this.invoice = invoices.find(x => x.ID === this.invoiceID)
-        this.invoicesLinks = this.invoice.PaymentLinks
+	getCurrentInvoice() {
+		// 	this.loading$.next(true)
+		// 	this.invoicesService
+		// 		.getInvoice({ID: this.invoiceID, includeLinks: true})
+		// 		.pipe(
+		// 			tap(data => {
+		// 				console.log('data :>> ', data)
+		// 			}),
+		// 			finalize(() => {
+		// 				this.loading$.next(false)
+		// 			})
+		// 		)
+		// 		.subscribe()
+		this.loading$.next(true)
+		return this.invoicesService
+			.getInvoices({
+				includeLinks: true
+			})
+			.pipe(
+				map(invoices => {
+					this.invoice = invoices.find(x => x.ID === this.invoiceID)
+					console.log('this.invoice :>> ', this.invoice)
+					this.invoicesLinks = this.invoice.PaymentLinks
+					console.log('this.invoicesLinks :>> ', this.invoicesLinks)
 
-        this.totalSumDutyDebtor = this.invoicesLinks.reduce((acc, link) => acc + link.Shipment.DutyDebtor, 0);
+					this.totalSumDutyDebtor = this.invoicesLinks.reduce(
+						(acc, link) => acc + link.Shipment.DutyDebtor,
+						0
+					)
+					console.log('this.totalSumDutyDebtor :>> ', this.totalSumDutyDebtor)
+					this.onPageChange(1)
+				}),
+				finalize(() => {
+					this.loading$.next(false)
+				})
+			)
+	}
 
-        this.onPageChange(1)
-      }),
-      finalize(() => {
-        this.loading$.next(false)
-      })
-    )
-  }
+	onPageChange(page: number) {
+		const startIndex = (page - 1) * this.PAGINATOR_ITEMS_PER_PAGE
+		const endIndex = startIndex + this.PAGINATOR_ITEMS_PER_PAGE
 
-  onPageChange(page: number) {
-    const startIndex = (page - 1) * this.PAGINATOR_ITEMS_PER_PAGE
-    const endIndex = startIndex + this.PAGINATOR_ITEMS_PER_PAGE
-
-    this.invoicesLinksVisible = this.invoicesLinks.slice(startIndex, endIndex)
-  }
+		this.invoicesLinksVisible = this.invoicesLinks.slice(startIndex, endIndex)
+	}
 }
