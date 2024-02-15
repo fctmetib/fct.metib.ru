f98083ce85672ccb342cbfa288e619cef2ef1999
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-02-15 20:05:27 +0600

    upd: demand.limit.drawer view

--- src/app/client/modules/demand-new/modules/demand-limit-drawer/demand-limit-drawer.component.ts
+++ src/app/client/modules/demand-new/modules/demand-limit-drawer/demand-limit-drawer.component.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core'
 import {MatDialogRef} from '@angular/material/dialog'
+import {FileDnd} from 'src/app/shared/ui-kit/drag-and-drop/interfaces/drop-box.interface'
+import {DocumentReq} from '../../../requests/interfaces/request.interface'
+import {extractBase64} from 'src/app/shared/services/tools.service'
 
 @Component({
 	selector: 'mib-demand-limit-drawer',
@@ -8,4 +11,17 @@ import {MatDialogRef} from '@angular/material/dialog'
 })
 export class DemandLimitDrawerComponent {
 	constructor(public dialogRef: MatDialogRef<DemandLimitDrawerComponent>) {}
+
+	data = 20000
+
+	onDocumentLoad({file, url}: FileDnd) {
+		const document: DocumentReq = {
+			Description: `description ${file.name}`,
+			DocumentTypeID: 40,
+			Title: file.name,
+			OwnerTypeID: 20,
+			Data: extractBase64(url)
+		}
+		// this.addDocument(document)
+	}
 }
--- src/app/client/modules/demand-new/modules/demand-limit-drawer/demand-limit-drawer.module.ts
+++ src/app/client/modules/demand-new/modules/demand-limit-drawer/demand-limit-drawer.module.ts
@@ -4,10 +4,30 @@ import {DemandLimitDrawerComponent} from './demand-limit-drawer.component'
 import {DrawerModule} from 'src/app/shared/ui-kit/drawer/drawer.module'
 import {SpacingModule} from 'src/app/shared/ui-kit/spacing/spacing.module'
 import {DemandLimitDrawerService} from './demand-limit-drawer.service'
+import {CashPanelModule} from 'src/app/shared/modules/cash-panel/cash-panel.module'
+import {InputModule} from 'src/app/shared/ui-kit/input/input.module'
+import {LabelModule} from 'src/app/shared/directives/label/label.module'
+import {TextareaModule} from 'src/app/shared/ui-kit/textarea/textarea.module'
+import {AutosizeModule} from 'ngx-autosize'
+import {InformationModule} from 'src/app/shared/ui-kit/information/information.module'
+import {MibDragAndDropModule} from 'src/app/shared/ui-kit/drag-and-drop/mib-drag-and-drop.module'
+import {ButtonModule} from 'src/app/shared/ui-kit/button/button.module'
 
 @NgModule({
 	declarations: [DemandLimitDrawerComponent],
-	imports: [CommonModule, DrawerModule, SpacingModule],
+	imports: [
+		CommonModule,
+		DrawerModule,
+		SpacingModule,
+		CashPanelModule,
+		InputModule,
+		LabelModule,
+		TextareaModule,
+		AutosizeModule,
+		InformationModule,
+		MibDragAndDropModule,
+		ButtonModule
+	],
 	providers: [DemandLimitDrawerService]
 })
 export class DemandLimitDrawerModule {}
