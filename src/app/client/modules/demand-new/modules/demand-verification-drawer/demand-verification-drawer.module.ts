3e7ca7eecbb80f668a679c5568a6270dee76c8d8
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-02-16 20:15:21 +0600

    add: demand.verification.drawer view

--- src/app/client/modules/demand-new/modules/demand-verification-drawer/demand-verification-drawer.module.ts
+++ src/app/client/modules/demand-new/modules/demand-verification-drawer/demand-verification-drawer.module.ts
@@ -4,10 +4,30 @@ import {DemandVerificationDrawerComponent} from './demand-verification-drawer.co
 import {SpacingModule} from 'src/app/shared/ui-kit/spacing/spacing.module'
 import {DrawerModule} from 'src/app/shared/ui-kit/drawer/drawer.module'
 import {DemandVerificationDrawerService} from './demand-verification-drawer.service'
+import {DropdownPointModule} from 'src/app/shared/ui-kit/dropdown-point/dropdown-point.module'
+import {CheckboxModule} from 'src/app/shared/ui-kit/checkbox/checkbox.module'
+import {InputModule} from 'src/app/shared/ui-kit/input/input.module'
+import {LabelModule} from 'src/app/shared/directives/label/label.module'
+import {ButtonModule} from 'src/app/shared/ui-kit/button/button.module'
+import {SelectModule} from 'src/app/shared/ui-kit/select/select.module'
+import {TextareaModule} from 'src/app/shared/ui-kit/textarea/textarea.module'
+import {AutosizeModule} from 'ngx-autosize'
 
 @NgModule({
 	declarations: [DemandVerificationDrawerComponent],
-	imports: [CommonModule, DrawerModule, SpacingModule],
+	imports: [
+		CommonModule,
+		DrawerModule,
+		SpacingModule,
+		DropdownPointModule,
+		CheckboxModule,
+		InputModule,
+		LabelModule,
+		ButtonModule,
+		SelectModule,
+		TextareaModule,
+		AutosizeModule
+	],
 	providers: [DemandVerificationDrawerService]
 })
 export class DemandVerificationDrawerModule {}
