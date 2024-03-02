5d3b537914356969cb1eed50e2460dc68954f17f
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-03-02 22:03:21 +0600

    add: footer.comp, basic

--- src/app/shared/modules/new-footer/new-footer.module.ts
+++ src/app/shared/modules/new-footer/new-footer.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
 import {NewFooterComponent} from './new-footer.component'
+import {HeaderModule} from '../header/header.module'
+import {SpacingModule} from '../../ui-kit/spacing/spacing.module'
+import {LinkModule} from '../../ui-kit/link/link.module'
+import {MediaModule} from '../../ui-kit/media/media.module'
 
 @NgModule({
 	declarations: [NewFooterComponent],
-	imports: [CommonModule],
+	imports: [CommonModule, HeaderModule, SpacingModule, LinkModule, MediaModule],
 	exports: [NewFooterComponent]
 })
 export class NewFooterModule {}
--- src/app/shared/ui-kit/link/link.component.ts
+++ src/app/shared/ui-kit/link/link.component.ts
@@ -1,32 +1,40 @@
-import {Component, ContentChildren, ElementRef, Input, QueryList, AfterContentInit, Output, EventEmitter} from '@angular/core';
-import { LinkSize, LinkType } from './interfaces/link.interface';
-import {RightIconDirective} from '../../directives/right-icon/right-icon.directive';
-import {LeftIconDirective} from '../../directives/left-icon/left-icon.directive';
+import {
+	Component,
+	ContentChildren,
+	ElementRef,
+	Input,
+	QueryList,
+	AfterContentInit,
+	Output,
+	EventEmitter
+} from '@angular/core'
+import {LinkSize, LinkType} from './interfaces/link.interface'
+import {RightIconDirective} from '../../directives/right-icon/right-icon.directive'
+import {LeftIconDirective} from '../../directives/left-icon/left-icon.directive'
 
 @Component({
-  selector: 'mib-link',
-  templateUrl: './link.component.html',
-  styleUrls: ['./link.component.scss']
+	selector: 'mib-link',
+	templateUrl: './link.component.html',
+	styleUrls: ['./link.component.scss']
 })
 export class LinkComponent implements AfterContentInit {
+	@ContentChildren(LeftIconDirective) leftIcons: QueryList<LeftIconDirective>
+	@ContentChildren(RightIconDirective) rightIcons: QueryList<RightIconDirective>
 
-  @ContentChildren(LeftIconDirective) leftIcons: QueryList<LeftIconDirective>;
-  @ContentChildren(RightIconDirective) rightIcons: QueryList<RightIconDirective>;
+	@Input() size: LinkSize = 'l'
+	@Input() type: LinkType = 'ghost-primary'
+	@Input() disabled: boolean = false
+	@Input() showUnderline: boolean = false
+	@Output() press = new EventEmitter()
 
-  @Input() size: LinkSize = 'l';
-  @Input() type: LinkType = 'ghost-primary';
-  @Input() disabled: boolean = false;
-  @Output() press = new EventEmitter()
+	ngAfterContentInit() {}
 
-  ngAfterContentInit() {
-  }
-
-  get classes() {
-    return {
-      [`link_${this.size}`]: true,
-      [`link_type-${this.type}`]: true,
-      'link_left-iconly': this.leftIcons.length,
-      'link_right-iconly': this.rightIcons.length
-    }
-  }
+	get classes() {
+		return {
+			[`link_${this.size}`]: true,
+			[`link_type-${this.type}`]: true,
+			'link_left-iconly': this.leftIcons.length,
+			'link_right-iconly': this.rightIcons.length
+		}
+	}
 }
