80dffa266261b99a620e923e933de87071d35397
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-03-03 19:07:55 +0600

    fin: footer.comp

--- src/app/shared/modules/new-footer/new-footer.module.ts
+++ src/app/shared/modules/new-footer/new-footer.module.ts
@@ -5,10 +5,21 @@ import {HeaderModule} from '../header/header.module'
 import {SpacingModule} from '../../ui-kit/spacing/spacing.module'
 import {LinkModule} from '../../ui-kit/link/link.module'
 import {MediaModule} from '../../ui-kit/media/media.module'
+import {IconModule} from '../../ui-kit/ref-icon/icon.module'
+import {RouterLink} from '@angular/router'
+import {ButtonModule} from '../../ui-kit/button/button.module'
 
 @NgModule({
 	declarations: [NewFooterComponent],
-	imports: [CommonModule, HeaderModule, SpacingModule, LinkModule, MediaModule],
+	imports: [
+		CommonModule,
+		SpacingModule,
+		LinkModule,
+		MediaModule,
+		IconModule,
+		RouterLink,
+		ButtonModule
+	],
 	exports: [NewFooterComponent]
 })
 export class NewFooterModule {}
