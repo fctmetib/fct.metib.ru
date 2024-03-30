62e8bbe5f2dedbc40c151fe0f3448e3f295a87c2
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-03-30 19:06:20 +0600

    fin: single.news.comp routing

--- src/app/public/pages/single-news/single-news.component.ts
+++ src/app/public/pages/single-news/single-news.component.ts
@@ -1,4 +1,10 @@
 import {Component} from '@angular/core'
+import {BehaviorSubject, finalize, map, switchMap, tap, zip} from 'rxjs'
+import {AdvancedNewsInterface} from '../../type/news.interface'
+import {NewsService} from '../../service/news.service'
+import {ToolsService} from 'src/app/shared/services/tools.service'
+import {ActivatedRoute, Router} from '@angular/router'
+import {Properties} from 'csstype'
 
 @Component({
 	selector: 'mib-single-news',
@@ -7,4 +13,49 @@ import {Component} from '@angular/core'
 })
 export class SingleNewsComponent {
 	public imageSrc: string = './assets/images/news/news-2.jpg'
+
+	public defaultSkeleton: Properties = {
+		borderRadius: '8px',
+		width: '560px',
+		height: '315px',
+		margin: '0 auto'
+	}
+
+	public loading$ = new BehaviorSubject<boolean>(false)
+	public newsNumberCount: number = 5
+	public getSingleNews: AdvancedNewsInterface[]
+
+	constructor(
+		private newsService: NewsService,
+		public toolsService: ToolsService,
+		private route: ActivatedRoute
+	) {}
+
+	ngOnInit(): void {
+		this.getCurrentNews()
+	}
+
+	public getCurrentNews() {
+		this.loading$.next(true)
+		this.newsService
+			.getNews(this.newsNumberCount)
+			.pipe(
+				switchMap(news =>
+					zip(
+						news.map(item =>
+							this.newsService
+								.getNewsImage(item.ID)
+								.pipe(map(image => ({...item, Image: image})))
+						)
+					).pipe(
+						tap(data => {
+							const id = Number(this.route.snapshot.paramMap.get('id'))
+							this.getSingleNews = data.filter(el => el.ID === id)
+						})
+					)
+				),
+				finalize(() => this.loading$.next(false))
+			)
+			.subscribe()
+	}
 }
