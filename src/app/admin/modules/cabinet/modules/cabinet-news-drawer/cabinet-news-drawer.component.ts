d26e3e5dea3b0c6908a681c4025becdd2930a694
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-04-23 19:20:26 +0600

    upd: crud drawers.lgc

--- src/app/admin/modules/cabinet/modules/cabinet-news-drawer/cabinet-news-drawer.component.ts
+++ src/app/admin/modules/cabinet/modules/cabinet-news-drawer/cabinet-news-drawer.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core'
 import {MatDialogRef} from '@angular/material/dialog'
+import {DrawerStateEnum} from 'src/app/shared/ui-kit/drawer/interfaces/drawer.interface'
+import {CabinetCreateNewsDrawerService} from '../cabinet-create-news-drawer/cabinet-create-news-drawer.service'
 
 @Component({
 	selector: 'mib-cabinet-news-drawer',
@@ -7,13 +9,25 @@ import {MatDialogRef} from '@angular/material/dialog'
 	styleUrls: ['./cabinet-news-drawer.component.scss']
 })
 export class CabinetNewsDrawerComponent {
-	constructor(public dialogRef: MatDialogRef<CabinetNewsDrawerComponent>) {}
+	constructor(
+		public dialogRef: MatDialogRef<CabinetNewsDrawerComponent>,
+		private сabinetCreateNewsDrawerService: CabinetCreateNewsDrawerService
+	) {}
 
 	editNews() {
 		console.log('edit news >>>')
+		this.dialogRef.close()
+		this.openCreateNewsDrawer()
 	}
 
 	removeNews() {
 		console.log('remove news >>>')
+	}
+
+	openCreateNewsDrawer() {
+		this.сabinetCreateNewsDrawerService
+			.open({state: DrawerStateEnum.CREATE})
+			.afterClosed()
+			.subscribe()
 	}
 }
