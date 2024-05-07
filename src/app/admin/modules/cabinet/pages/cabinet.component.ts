8e8f68613bd0e0cf2fb63f74db392bca6ecb6797
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-05-07 19:22:51 +0600

    feat: implement pagination, basic

--- src/app/admin/modules/cabinet/pages/cabinet.component.ts
+++ src/app/admin/modules/cabinet/pages/cabinet.component.ts
@@ -1,30 +1,17 @@
-import {Paginator} from 'primeng/paginator'
 import {
 	BehaviorSubject,
-	Observable,
-	Subscription,
 	filter,
 	finalize,
-	from,
 	map,
-	of,
 	switchMap,
 	takeUntil,
 	tap,
 	zip
 } from 'rxjs'
-import {Component, OnInit, ViewChild} from '@angular/core'
-import {PageStoreService} from 'src/app/admin/shared/services/page-store.service'
-import {NewsInterface} from 'src/app/admin/shared/types/news.interface'
-import {FormBuilder, FormGroup} from '@angular/forms'
-import {DialogService, DynamicDialogRef} from 'primeng/dynamicdialog'
-import {CreateNewsDialogComponent} from '../components/create-news-dialog/create-news-dialog.component'
-import {CabinetNewsDrawerComponent} from '../modules/cabinet-news-drawer/cabinet-news-drawer.component'
+import {Component, OnInit} from '@angular/core'
 import {MatDialog, MatDialogRef} from '@angular/material/dialog'
-import {DrawerStateEnum} from 'src/app/shared/ui-kit/drawer/interfaces/drawer.interface'
 import {CabinetNewsDrawerService} from '../modules/cabinet-news-drawer/cabinet-news-drawer.service'
 import {CabinetCreateNewsDrawerService} from '../modules/cabinet-create-news-drawer/cabinet-create-news-drawer.service'
-import {CabinetEditNewsDrawerService} from '../modules/cabinet-edit-news-drawer/cabinet-edit-news-drawer.service'
 import {AdvancedNewsInterface} from 'src/app/public/type/news.interface'
 import {NewsService} from 'src/app/public/service/news.service'
 import {Properties} from 'csstype'
@@ -48,8 +35,7 @@ export class CabinetComponent implements OnInit {
 		link: '/news/1'
 	}
 
-	public newsNumberCount: number = 6
-	public getAdvancedNews: AdvancedNewsInterface[]
+	public newsNumberCount: number = 12
 	public getSortedNews: AdvancedNewsInterface[]
 
 	public skeleton: Properties = {
@@ -88,6 +74,18 @@ export class CabinetComponent implements OnInit {
 		// get news end
 	}
 
+	onPageChange(page: number) {
+		console.log('page HALO:>> ', page)
+		this.currentPage$.next(page)
+		const startIndex = (page - 1) * this.PAGINATOR_ITEMS_PER_PAGE
+		const endIndex = startIndex + this.PAGINATOR_ITEMS_PER_PAGE
+
+		console.log('this.currentPage$:>> ', this.currentPage$.value)
+		console.log('startIndex :>> ', startIndex)
+		console.log('endIndex :>> ', endIndex)
+		this.dispalyNews$.next(this.getSortedNews.slice(startIndex, endIndex))
+	}
+
 	public getCurrentNews() {
 		this.loading$.next(true)
 		this.newsService
@@ -102,10 +100,9 @@ export class CabinetComponent implements OnInit {
 						)
 					).pipe(
 						tap(data => {
-							this.getAdvancedNews = data
+							this.getSortedNews = data
 							this.onPageChange(this.currentPage$.value)
 							console.log('data :>> ', data)
-							this.dispalyNews$.next(this.getAdvancedNews)
 						})
 					)
 				),
@@ -115,18 +112,20 @@ export class CabinetComponent implements OnInit {
 	}
 
 	sortNewsByDate(order: 'new' | 'old') {
-		if (this.getAdvancedNews) {
-			const temp = [...this.getAdvancedNews]
+		if (this.getSortedNews) {
+			const temp = [...this.getSortedNews]
 			if (order === 'new') {
 				this.getSortedNews = temp.sort(
 					(a, b) => new Date(b.Date).getTime() - new Date(a.Date).getTime()
 				)
 				this.dispalyNews$.next(this.getSortedNews)
+				this.onPageChange(this.currentPage$.value)
 			} else if (order === 'old') {
 				this.getSortedNews = temp.sort(
 					(a, b) => new Date(a.Date).getTime() - new Date(b.Date).getTime()
 				)
 				this.dispalyNews$.next(this.getSortedNews)
+				this.onPageChange(this.currentPage$.value)
 			}
 		}
 	}
@@ -165,254 +164,4 @@ export class CabinetComponent implements OnInit {
 			.afterClosed()
 			.subscribe()
 	}
-
-	onPageChange(page: number) {
-		console.log('page HALO:>> ', page)
-		this.currentPage$.next(page)
-		const startIndex = (page - 1) * this.PAGINATOR_ITEMS_PER_PAGE
-		const endIndex = startIndex + this.PAGINATOR_ITEMS_PER_PAGE
-
-		console.log('this.currentPage$:>> ', this.currentPage$.value)
-	}
 }
-
-//   public filterForm: FormGroup;
-//   public newsListOriginal: NewsInterface[] = [];
-//   public newsListFiltered: NewsInterface[] = [];
-//   public newsListDisplayed: NewsInterface[] = [];
-
-//   @ViewChild('paginator', { static: false }) paginator!: Paginator;
-//   public paginationPage: number = 0;
-//   public paginationRows: number = 10;
-
-//   ref: DynamicDialogRef;
-
-//   private subscription$: Subscription = new Subscription();
-
-//   constructor(
-//     private pageStoreService: PageStoreService,
-//     private newsService: NewsService,
-//     public dialogService: DialogService,
-//     private formBuilder: FormBuilder
-//   ) {}
-
-//   ngOnInit() {
-//     this.pageStoreService.setPage({
-//       header: 'Панель Администратора',
-//       description: 'Добро пожаловать в панель администратора!',
-//     });
-
-//     this.fetch();
-
-//     this.filterForm = this.formBuilder.group({
-//       search: '',
-//     });
-
-//     this.onChanges();
-//   }
-
-//   public handleRemove(newsID: string) {
-//     this.subscription$.add(
-//       this.newsService.removeNewsItem(newsID).subscribe((removeResponse) => {
-//         this.fetch();
-//       })
-//     );
-//   }
-
-//   public openCreateNews(newsItem?: NewsInterface) {
-//     this.ref = this.dialogService.open(CreateNewsDialogComponent, {
-//       header: 'Создание новости',
-//       width: '50%',
-//       contentStyle: { 'max-height': '550px', overflow: 'auto' },
-//       baseZIndex: 10000,
-//       data: newsItem,
-//     });
-
-//     this.subscription$.add(
-//       this.ref.onClose.subscribe(() => {
-//         this.fetch();
-//       })
-//     );
-//   }
-
-//   public paginate(event?): void {
-//     this.newsListDisplayed = [];
-//     if (event) {
-//       this.paginationPage = event.page;
-
-//       let currentIndex = event.page * event.rows;
-//       this.newsListDisplayed = this.newsListFiltered.slice(
-//         currentIndex,
-//         currentIndex + event.rows
-//       );
-//     } else {
-//       let currentIndex = this.paginationPage * this.paginationRows;
-//       this.updateCurrentPage(currentIndex);
-
-//       this.newsListDisplayed = this.newsListFiltered.slice(
-//         currentIndex,
-//         currentIndex + this.paginationRows
-//       );
-//     }
-//   }
-
-//   private fetch() {
-//     this.newsService.getNewsList().subscribe((newsListResponse) => {
-//       this.newsListOriginal = newsListResponse;
-//       this.newsListFiltered = newsListResponse;
-//       this.paginate()
-//     });
-//   }
-
-//   private onChanges(): void {
-//     this.subscription$.add(
-//       this.filterForm.valueChanges.subscribe((value) => {
-//         this.newsListFiltered = this.newsListOriginal.filter(
-//           (newsItem) =>
-//             newsItem.Title.includes(value.search) ||
-//             newsItem.Text.includes(value.search)
-//         );
-//         this.paginate();
-//       })
-//     );
-//   }
-
-//   private updateCurrentPage(currentPage: number): void {
-//     this.paginator.changePage(currentPage);
-//   }
-
-//   ngOnDestroy() {
-//     if (this.ref) {
-//       this.ref.close();
-//     }
-//     this.subscription$.unsubscribe();
-//   }
-// }
-
-// import { Paginator } from 'primeng/paginator';
-// import { NewsService } from './../../../../shared/services/news.service';
-// import { Subscription } from 'rxjs';
-// import { Component, OnInit, ViewChild } from '@angular/core';
-// import { PageStoreService } from 'src/app/admin/shared/services/page-store.service';
-// import { NewsInterface } from 'src/app/admin/shared/types/news.interface';
-// import { FormBuilder, FormGroup } from '@angular/forms';
-// import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
-// import { CreateNewsDialogComponent } from '../create-news-dialog/create-news-dialog.component';
-
-// @Component({
-//   selector: 'cabinet',
-//   templateUrl: './cabinet.component.html',
-// })
-// export class CabinetComponent implements OnInit {
-//   public filterForm: FormGroup;
-//   public newsListOriginal: NewsInterface[] = [];
-//   public newsListFiltered: NewsInterface[] = [];
-//   public newsListDisplayed: NewsInterface[] = [];
-
-//   @ViewChild('paginator', { static: false }) paginator!: Paginator;
-//   public paginationPage: number = 0;
-//   public paginationRows: number = 10;
-
-//   ref: DynamicDialogRef;
-
-//   private subscription$: Subscription = new Subscription();
-
-//   constructor(
-//     private pageStoreService: PageStoreService,
-//     private newsService: NewsService,
-//     public dialogService: DialogService,
-//     private formBuilder: FormBuilder
-//   ) {}
-
-//   ngOnInit() {
-//     this.pageStoreService.setPage({
-//       header: 'Панель Администратора',
-//       description: 'Добро пожаловать в панель администратора!',
-//     });
-
-//     this.fetch();
-
-//     this.filterForm = this.formBuilder.group({
-//       search: '',
-//     });
-
-//     this.onChanges();
-//   }
-
-//   public handleRemove(newsID: string) {
-//     this.subscription$.add(
-//       this.newsService.removeNewsItem(newsID).subscribe((removeResponse) => {
-//         this.fetch();
-//       })
-//     );
-//   }
-
-//   public openCreateNews(newsItem?: NewsInterface) {
-//     this.ref = this.dialogService.open(CreateNewsDialogComponent, {
-//       header: 'Создание новости',
-//       width: '50%',
-//       contentStyle: { 'max-height': '550px', overflow: 'auto' },
-//       baseZIndex: 10000,
-//       data: newsItem,
-//     });
-
-//     this.subscription$.add(
-//       this.ref.onClose.subscribe(() => {
-//         this.fetch();
-//       })
-//     );
-//   }
-
-//   public paginate(event?): void {
-//     this.newsListDisplayed = [];
-//     if (event) {
-//       this.paginationPage = event.page;
-
-//       let currentIndex = event.page * event.rows;
-//       this.newsListDisplayed = this.newsListFiltered.slice(
-//         currentIndex,
-//         currentIndex + event.rows
-//       );
-//     } else {
-//       let currentIndex = this.paginationPage * this.paginationRows;
-//       this.updateCurrentPage(currentIndex);
-
-//       this.newsListDisplayed = this.newsListFiltered.slice(
-//         currentIndex,
-//         currentIndex + this.paginationRows
-//       );
-//     }
-//   }
-
-//   private fetch() {
-//     this.newsService.getNewsList().subscribe((newsListResponse) => {
-//       this.newsListOriginal = newsListResponse;
-//       this.newsListFiltered = newsListResponse;
-//       this.paginate()
-//     });
-//   }
-
-//   private onChanges(): void {
-//     this.subscription$.add(
-//       this.filterForm.valueChanges.subscribe((value) => {
-//         this.newsListFiltered = this.newsListOriginal.filter(
-//           (newsItem) =>
-//             newsItem.Title.includes(value.search) ||
-//             newsItem.Text.includes(value.search)
-//         );
-//         this.paginate();
-//       })
-//     );
-//   }
-
-//   private updateCurrentPage(currentPage: number): void {
-//     this.paginator.changePage(currentPage);
-//   }
-
-//   ngOnDestroy() {
-//     if (this.ref) {
-//       this.ref.close();
-//     }
-//     this.subscription$.unsubscribe();
-//   }
-// }
