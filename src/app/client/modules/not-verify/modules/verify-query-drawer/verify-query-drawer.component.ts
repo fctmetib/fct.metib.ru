90bf830bc02d2b046b7001f632a2c9c75db0da45
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-05-16 20:48:18 +0600

    feat: add verify.query drawer

--- src/app/client/modules/not-verify/modules/verify-query-drawer/verify-query-drawer.component.ts
+++ src/app/client/modules/not-verify/modules/verify-query-drawer/verify-query-drawer.component.ts
@@ -1,6 +1,11 @@
 import {Component, Inject} from '@angular/core'
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog'
+import {FileDnd} from 'src/app/shared/ui-kit/drag-and-drop/interfaces/drop-box.interface'
 import {DrawerData} from 'src/app/shared/ui-kit/drawer/interfaces/drawer.interface'
+import {DocumentReq} from '../../../requests/interfaces/request.interface'
+import {extractBase64} from 'src/app/shared/services/tools.service'
+import {ToasterService} from 'src/app/shared/services/common/toaster.service'
+import {BehaviorSubject} from 'rxjs'
 
 @Component({
 	selector: 'mib-verify-query-drawer',
@@ -8,8 +13,81 @@ import {DrawerData} from 'src/app/shared/ui-kit/drawer/interfaces/drawer.interfa
 	styleUrls: ['./verify-query-drawer.component.scss']
 })
 export class VerifyQueryDrawerComponent {
+	public progres$ = new BehaviorSubject<number>(1)
+	public progress: number = 1
+	public maxPage: number = 4
+	public pageCount: number = 1
+
 	constructor(
 		public dialogRef: MatDialogRef<VerifyQueryDrawerComponent>,
+		private toaster: ToasterService,
 		@Inject(MAT_DIALOG_DATA) public data: DrawerData
 	) {}
+
+	public nextPage() {
+		if (this.pageCount >= 1 && this.pageCount <= this.maxPage - 1) {
+			this.progress = this.progres$.value + 1
+			this.progres$.next(this.progress)
+			this.pageCount = this.progress
+			console.log('next', this.progress)
+		} else {
+			return
+		}
+	}
+
+	public prevPage() {
+		if (this.pageCount >= 2 && this.pageCount <= this.maxPage) {
+			this.progress = this.progres$.value - 1
+			this.progres$.next(this.progress)
+			this.pageCount = this.progress
+			console.log('prev', this.progress)
+		} else {
+			return
+		}
+	}
+
+	public onDocumentLoad({file, url}: FileDnd) {
+		const document: DocumentReq = {
+			Description: `description ${file.name}`,
+			DocumentTypeID: 40,
+			Title: file.name,
+			OwnerTypeID: 20,
+			Data: extractBase64(url)
+		}
+		// this.addDocument(document)
+	}
+
+	public submitData() {
+		this.toaster.show(
+			'failure',
+			'Функционал в разработке!',
+			'',
+			true,
+			false,
+			3000
+		)
+		// this.dialogRef.close()
+	}
+
+	public confirmIds() {
+		this.toaster.show(
+			'failure',
+			'Функционал в разработке!',
+			'',
+			true,
+			false,
+			3000
+		)
+	}
+
+	public downloadFile() {
+		this.toaster.show(
+			'failure',
+			'Функционал в разработке!',
+			'',
+			true,
+			false,
+			3000
+		)
+	}
 }
