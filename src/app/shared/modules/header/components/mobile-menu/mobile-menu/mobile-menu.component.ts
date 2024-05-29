8506885e6984f86846e30e34b3ac75d321a8feef
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-05-29 20:17:42 +0600

    feat: add mobile.client menu

--- src/app/shared/modules/header/components/mobile-menu/mobile-menu/mobile-menu.component.ts
+++ src/app/shared/modules/header/components/mobile-menu/mobile-menu/mobile-menu.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core'
+import {AuthService} from 'src/app/auth/services/auth.service'
 
 @Component({
 	selector: 'mib-mobile-menu',
@@ -6,8 +7,47 @@ import {Component, Input} from '@angular/core'
 	styleUrls: ['./mobile-menu.component.scss']
 })
 export class MobileMenuComponent {
+	menuClient = [
+		{
+			links: [
+				{name: 'Кабинет', link: '/client/cabinet'},
+				{name: 'Заявки', link: '/client/requests'},
+				{name: 'Свободная задолженность', link: '/client/freeduty'}
+			]
+		},
+		{
+			links: [
+				{name: 'Платежи', link: '/client/invoices'},
+				{name: 'Просрочка платежа', link: '/client/delays'},
+				{name: 'Договоры', link: '/client/contracts'}
+			]
+		},
+		{
+			links: [
+				{name: 'Отчеты', link: '/client/reports'},
+				{name: 'Электронные документы', link: '/client/documents'},
+				{name: 'Запросы', link: '/client/demand'}
+			]
+		}
+	]
+	menuAdmin = [
+		{name: 'Новости', link: '/admin/cabinet'},
+		{name: 'Организации', link: '/admin/organizations'},
+		{name: 'Пользователи', link: '/admin/users'},
+		{name: 'Бизнес-тесты', link: '/admin/tests'}
+	]
+	menuVerify = [{name: 'Запросы', link: '/client/not-verify'}]
+
+	menuMain = [
+		{name: 'Главная', link: '/'},
+		{name: 'Тарифы', link: '/tariffs'},
+		{name: 'Клиентам', link: '/clients'},
+		{name: 'Контакты', link: '/contacts'}
+	]
 	@Input() isClient: boolean = false
 	public isOpen: Boolean = false
+
+	constructor(public authService: AuthService) {}
 
 	public onBurger() {
 		this.isOpen = !this.isOpen
@@ -15,5 +55,9 @@ export class MobileMenuComponent {
 
 	public closeBurgerMenu() {
 		this.isOpen = !this.isOpen
+	}
+
+	logout() {
+		this.authService.logout()
 	}
 }
