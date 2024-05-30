125c3cb1dfe90b4f22946ef6eb02a101d76055b8
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-05-30 15:57:23 +0600

    fix: mob.header displaying

--- src/app/shared/modules/header/components/mobile-menu/mobile-menu/mobile-menu.component.ts
+++ src/app/shared/modules/header/components/mobile-menu/mobile-menu/mobile-menu.component.ts
@@ -55,50 +55,10 @@ export class MobileMenuComponent {
 			]
 		}
 	]
-	// menuClient = [
-	// 	{
-	// 		links: [
-	// 			{name: 'Кабинет', link: '/client/cabinet'},
-	// 			{name: 'Заявки', link: '/client/requests'},
-	// 			{name: 'Свободная задолженность', link: '/client/freeduty'}
-	// 		]
-	// 	},
-	// 	{
-	// 		links: [
-	// 			{name: 'Платежи', link: '/client/invoices'},
-	// 			{name: 'Просрочка платежа', link: '/client/delays'},
-	// 			{name: 'Договоры', link: '/client/contracts'}
-	// 		]
-	// 	},
-	// 	{
-	// 		links: [
-	// 			{name: 'Отчеты', link: '/client/reports'},
-	// 			{name: 'Электронные документы', link: '/client/documents'},
-	// 			{name: 'Запросы', link: '/client/demand'}
-	// 		]
-	// 	}
-	// ]
-
-	// menuAdmin = [
-	// 	{name: 'Новости', link: '/admin/cabinet'},
-	// 	{name: 'Организации', link: '/admin/organizations'},
-	// 	{name: 'Пользователи', link: '/admin/users'},
-	// 	{name: 'Бизнес-тесты', link: '/admin/tests'}
-	// ]
-
-	// menuVerify = [{name: 'Запросы', link: '/client/not-verify'}]
-
-	// menuMain = [
-	// 	{name: 'Главная', link: '/'},
-	// 	{name: 'Тарифы', link: '/tariffs'},
-	// 	{name: 'Клиентам', link: '/clients'},
-	// 	{name: 'Контакты', link: '/contacts'}
-	// ]
 
 	@Input() isClient: boolean = false
 	@Input() isAdmin: boolean = false
 	public isOpen: Boolean = false
-	// isAdmin: boolean = false
 	isVerify: boolean = false
 	isLogged: boolean = false
 	isAuthenticated: boolean = false
@@ -106,14 +66,8 @@ export class MobileMenuComponent {
 	constructor(public authService: AuthService) {}
 
 	ngOnInit(): void {
-		// this.isAdmin = this.authService.isUserAdmin()
-		console.log('this.isAdmin :>> ', this.isAdmin)
 		this.isVerify = this.authService.isUserVerified()
-		console.log('this.isVerify :>> ', this.isVerify)
 		this.isLogged = this.authService.isUserLoggedIn
-		console.log('this.isLogged :>> ', this.isLogged)
-		this.isAuthenticated = this.authService.isAuthenticated()
-		console.log('this.isClient :>> ', this.isClient)
 	}
 
 	public onBurger() {
