c31c1ba3bb982851a54cc0bbb74182ebc98fd4f8
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-06-07 21:08:41 +0600

    feat: basic table.mob view

--- src/app/shared/ui-kit/table/components/table-quick-action-island/table-quick-action-island.component.ts
+++ src/app/shared/ui-kit/table/components/table-quick-action-island/table-quick-action-island.component.ts
@@ -1,17 +1,35 @@
-import {Component, Input} from '@angular/core'
+import {Component, Input, OnDestroy, OnInit} from '@angular/core'
 import {TableFooterComponent} from '../table-footer/table-footer.component'
+import {BreakpointObserverService} from 'src/app/shared/services/common/breakpoint-observer.service'
+import {Subscription} from 'rxjs'
 
 @Component({
 	selector: 'mib-table-quick-action-island',
 	templateUrl: './table-quick-action-island.component.html',
 	styleUrls: ['./table-quick-action-island.component.scss']
 })
-export class TableQuickActionIslandComponent {
+export class TableQuickActionIslandComponent implements OnInit, OnDestroy {
 	@Input() showRemove: boolean = true
+	public isDesktop: boolean = false
 
-	constructor(public footer: TableFooterComponent) {}
+	private subscriptions = new Subscription()
+
+	constructor(
+		public footer: TableFooterComponent,
+		public breakpointService: BreakpointObserverService
+	) {}
+
+	ngOnInit(): void {
+		this.subscriptions = this.breakpointService
+			.isDesktop()
+			.subscribe(b => (this.isDesktop = b))
+	}
 
 	get table() {
 		return this.footer.table
+	}
+
+	ngOnDestroy(): void {
+		this.subscriptions.unsubscribe()
 	}
 }
--- src/app/shared/ui-kit/table/components/table-row/table-row.component.ts
+++ src/app/shared/ui-kit/table/components/table-row/table-row.component.ts
@@ -1,10 +1,19 @@
-import {Component, ContentChildren, Input, QueryList} from '@angular/core'
+import {
+	Component,
+	ContentChildren,
+	Input,
+	OnDestroy,
+	OnInit,
+	QueryList
+} from '@angular/core'
 import {DeviceType} from '../../../../interfaces/shared.interface'
 import {TableCellComponent} from '../table-cell/table-cell.component'
 import {TableRowAnimationService} from '../../services/table-row-animation.service'
 import {TableComponent} from '../../table.component'
 import {AnimationService} from '../../../../animations/animations.service'
 import {ToolsService} from '../../../../services/tools.service'
+import {Subscription} from 'rxjs'
+import {BreakpointObserverService} from 'src/app/shared/services/common/breakpoint-observer.service'
 
 export const TABLE_ROW_ANIMATION_CONFIG = {
 	translateDistance: '-3%',
@@ -21,20 +30,31 @@ export const TABLE_ROW_ANIMATION_CONFIG = {
 		new AnimationService().generateAnimation(TABLE_ROW_ANIMATION_CONFIG)
 	]
 })
-export class TableRowComponent {
+export class TableRowComponent implements OnInit, OnDestroy {
 	@ContentChildren(TableCellComponent) cells: QueryList<TableCellComponent>
 	@Input() rowId: number // Уникальный идентификатор для строки
 	@Input() rowStatus: string
 	@Input() device: DeviceType = 'desktop'
 	@Input() underlined: boolean = false
 
+	public isDesktop: boolean = false
+
+	private subscriptions = new Subscription()
+
 	id: string = this.toolsService.generateId()
 
 	constructor(
 		private tableComponent: TableComponent,
 		private tableRowAnimationService: TableRowAnimationService,
-		private toolsService: ToolsService
+		private toolsService: ToolsService,
+		public breakpointService: BreakpointObserverService
 	) {}
+
+	ngOnInit(): void {
+		this.subscriptions = this.breakpointService
+			.isDesktop()
+			.subscribe(b => (this.isDesktop = b))
+	}
 
 	get animationState() {
 		return this.tableRowAnimationService.getAnimationState(this.rowId)
@@ -50,5 +70,14 @@ export class TableRowComponent {
 
 	get state() {
 		return Boolean(this.cell?.state)
+	}
+
+	openModal() {
+		event.stopPropagation()
+		console.log('OPEN MODAL')
+	}
+
+	ngOnDestroy(): void {
+		this.subscriptions.unsubscribe()
 	}
 }
