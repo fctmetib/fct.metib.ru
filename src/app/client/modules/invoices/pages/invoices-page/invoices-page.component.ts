de8e495773b12342fedd472464e65383a2422f22
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-06-11 14:40:47 +0600

    refactor: mobile.table view structure

--- src/app/client/modules/invoices/pages/invoices-page/invoices-page.component.ts
+++ src/app/client/modules/invoices/pages/invoices-page/invoices-page.component.ts
@@ -1,113 +1,143 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, OnDestroy, OnInit} from '@angular/core'
 import {Properties} from 'csstype'
-import {BehaviorSubject, finalize, merge, switchMap, tap} from 'rxjs'
+import {
+	BehaviorSubject,
+	Subscription,
+	finalize,
+	merge,
+	switchMap,
+	tap
+} from 'rxjs'
 import {InvoicesReq, InvoicesService} from '../../services/invoices.service'
 import {ClientInvoice} from '../../interfaces/client.invoice'
 import {InvoiceDrawerService} from '../../modules/invoice-drawer/invoice-drawer.service'
 import {DatesService} from 'src/app/shared/services/dates.service'
 import {ToolsService} from 'src/app/shared/services/tools.service'
 import {FormControl} from '@angular/forms'
-import {takeUntil} from 'rxjs/operators';
-import {AutoUnsubscribeService} from '../../../../../shared/services/auto-unsubscribe.service';
+import {takeUntil} from 'rxjs/operators'
+import {AutoUnsubscribeService} from '../../../../../shared/services/auto-unsubscribe.service'
+import {BreakpointObserverService} from 'src/app/shared/services/common/breakpoint-observer.service'
 
 @Component({
-  selector: 'mib-invoices-page',
-  templateUrl: './invoices-page.component.html',
-  styleUrls: ['./invoices-page.component.scss'],
+	selector: 'mib-invoices-page',
+	templateUrl: './invoices-page.component.html',
+	styleUrls: ['./invoices-page.component.scss']
 })
-export class InvoicesPageComponent implements OnInit {
-  public loading$ = new BehaviorSubject<boolean>(false)
+export class InvoicesPageComponent implements OnInit, OnDestroy {
+	public loading$ = new BehaviorSubject<boolean>(false)
 
-  public skeletonWithoutUnderline: Properties = {
-    height: '48px',
-    width: '100%'
-  }
-  public skeleton: Properties = {
-    ...this.skeletonWithoutUnderline,
-    borderBottom: '1px solid var(--wgr-tertiary)'
-  }
+	public skeletonWithoutUnderline: Properties = {
+		height: '48px',
+		width: '100%'
+	}
+	public skeleton: Properties = {
+		...this.skeletonWithoutUnderline,
+		borderBottom: '1px solid var(--wgr-tertiary)'
+	}
 
-  public PAGINATOR_ITEMS_PER_PAGE = 7
-  public PAGINATOR_PAGE_TO_SHOW = 5
-  public currentPage$ = new BehaviorSubject<number>(1)
+	public PAGINATOR_ITEMS_PER_PAGE = 7
+	public PAGINATOR_PAGE_TO_SHOW = 5
+	public currentPage$ = new BehaviorSubject<number>(1)
 
-  public selectedRequestCount: number = 0
-  public severalRequestsChecked: boolean = false
+	public selectedRequestCount: number = 0
+	public severalRequestsChecked: boolean = false
 
-  public invoices: ClientInvoice[] = []
-  public invoicesVisible: ClientInvoice[] = []
+	public invoices: ClientInvoice[] = []
+	public invoicesVisible: ClientInvoice[] = []
 
-  public dateFrom = new FormControl<string>('')
-  public dateTo = new FormControl<string>('')
+	public dateFrom = new FormControl<string>('')
+	public dateTo = new FormControl<string>('')
 
-  constructor(
-    public invoicesService: InvoicesService,
-    private invoiceDrawerService: InvoiceDrawerService,
-    public datesService: DatesService,
-    public toolsService: ToolsService,
-    private au: AutoUnsubscribeService
-  ) {
-  }
+	public isDesktop: boolean = false
+	private subscriptions = new Subscription()
 
-  ngOnInit(): void {
-    this.watchForms()
+	constructor(
+		public invoicesService: InvoicesService,
+		private invoiceDrawerService: InvoiceDrawerService,
+		public datesService: DatesService,
+		public toolsService: ToolsService,
+		private au: AutoUnsubscribeService,
+		public breakpointService: BreakpointObserverService
+	) {}
 
-    const {dateFrom, dateTo} = this.datesService.convertDatesInObjectToInput({
-      dateFrom: this.toolsService
-        .subtractFromDate(new Date(), {days: 7})
-        .toISOString(),
-      dateTo: new Date().toISOString()
-    })
-    this.dateFrom.setValue(dateFrom, {emitEvent: false})
-    this.dateTo.setValue(dateTo)
+	ngOnInit(): void {
+		this.watchForms()
 
-    this.loadInvoicesData().subscribe();
-  }
+		this.subscriptions = this.breakpointService
+			.isDesktop()
+			.subscribe(b => (this.isDesktop = b))
 
-  loadInvoicesData() {
-    const req: InvoicesReq = {
-    }
+		const {dateFrom, dateTo} = this.datesService.convertDatesInObjectToInput({
+			dateFrom: this.toolsService
+				.subtractFromDate(new Date(), {days: 7})
+				.toISOString(),
+			dateTo: new Date().toISOString()
+		})
+		this.dateFrom.setValue(dateFrom, {emitEvent: false})
+		this.dateTo.setValue(dateTo)
 
-    const setOptionalDate = (date: string, key: string) => {
-      if (date) req[key] = new Date(date).toISOString()
-    }
+		this.loadInvoicesData().subscribe()
+	}
 
-    setOptionalDate(this.dateFrom.value, 'dateFrom')
-    setOptionalDate(this.dateTo.value, 'dateTo')
+	loadInvoicesData() {
+		const req: InvoicesReq = {}
 
-    this.loading$.next(true)
-    return this.invoicesService.getInvoices(req).pipe(
-      tap(data => {
-        this.invoices = data
-      }),
-      finalize(() => this.loading$.next(false))
-    )
-  }
+		const setOptionalDate = (date: string, key: string) => {
+			if (date) req[key] = new Date(date).toISOString()
+		}
 
-  openDrawer(invoiceId: number) {
-    this.invoiceDrawerService.open({
-      data: {invoiceId: invoiceId}
-    })
-  }
+		setOptionalDate(this.dateFrom.value, 'dateFrom')
+		setOptionalDate(this.dateTo.value, 'dateTo')
 
-  onPageChange(page: number) {
-    this.currentPage$.next(page)
+		this.loading$.next(true)
+		return this.invoicesService.getInvoices(req).pipe(
+			tap(data => {
+				this.invoices = data
+			}),
+			finalize(() => this.loading$.next(false))
+		)
+	}
 
-    const startIndex = (page - 1) * this.PAGINATOR_ITEMS_PER_PAGE
-    const endIndex = startIndex + this.PAGINATOR_ITEMS_PER_PAGE
+	openDrawer(invoiceId: number) {
+		this.invoiceDrawerService.open({
+			data: {invoiceId: invoiceId}
+		})
+	}
 
-    this.invoicesVisible = this.invoices.slice(startIndex, endIndex)
-  }
+	openModal(data) {
+		console.log('OPEN MODAL .. YEAH', data)
+	}
 
+	prev() {
+		console.log('prev')
+	}
 
-  private watchForms() {
-    merge(this.dateFrom.valueChanges, this.dateTo.valueChanges)
-      .pipe(
-        switchMap(() => this.loadInvoicesData()),
-        tap(() => {
-          this.onPageChange(1)
-        }),
-        takeUntil(this.au.destroyer)
-      ).subscribe()
-  }
+	next() {
+		console.log('next')
+	}
+
+	onPageChange(page: number) {
+		this.currentPage$.next(page)
+
+		const startIndex = (page - 1) * this.PAGINATOR_ITEMS_PER_PAGE
+		const endIndex = startIndex + this.PAGINATOR_ITEMS_PER_PAGE
+
+		this.invoicesVisible = this.invoices.slice(startIndex, endIndex)
+	}
+
+	private watchForms() {
+		merge(this.dateFrom.valueChanges, this.dateTo.valueChanges)
+			.pipe(
+				switchMap(() => this.loadInvoicesData()),
+				tap(() => {
+					this.onPageChange(1)
+				}),
+				takeUntil(this.au.destroyer)
+			)
+			.subscribe()
+	}
+
+	ngOnDestroy(): void {
+		this.subscriptions.unsubscribe()
+	}
 }
--- src/app/shared/ui-kit/table/components/table-head/table-head.component.ts
+++ src/app/shared/ui-kit/table/components/table-head/table-head.component.ts
@@ -1,15 +1,15 @@
-import {Component, Input} from '@angular/core';
-import {TableCellSize} from '../table-cell/interfaces/table-cell.interface';
+import {Component, Input} from '@angular/core'
+import {TableCellSize} from '../table-cell/interfaces/table-cell.interface'
 
 @Component({
-  selector: 'mib-table-head',
-  templateUrl: './table-head.component.html',
-  styleUrls: ['./table-head.component.scss']
+	selector: 'mib-table-head',
+	templateUrl: './table-head.component.html',
+	styleUrls: ['./table-head.component.scss']
 })
 export class TableHeadComponent {
-  @Input() set size(value: TableCellSize) {
-    this._size = value;
-  }
+	@Input() set size(value: TableCellSize) {
+		this._size = value
+	}
 
-  public _size: TableCellSize = 'm'
+	public _size: TableCellSize = 'm'
 }
