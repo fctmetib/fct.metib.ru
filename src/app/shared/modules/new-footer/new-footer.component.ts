3b515d7928ed745189908d602f70aaa09b39ba0c
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-07-02 19:14:54 +0600

    feat: landing.footer mobile

--- src/app/shared/modules/new-footer/new-footer.component.ts
+++ src/app/shared/modules/new-footer/new-footer.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core'
+import {Subscription} from 'rxjs'
+import {BreakpointObserverService} from '../../services/common/breakpoint-observer.service'
 
 @Component({
-  selector: 'mib-new-footer',
-  templateUrl: './new-footer.component.html',
-  styleUrls: ['./new-footer.component.scss']
+	selector: 'mib-new-footer',
+	templateUrl: './new-footer.component.html',
+	styleUrls: ['./new-footer.component.scss']
 })
-export class NewFooterComponent {
+export class NewFooterComponent implements OnInit, OnDestroy {
+	public isDesktop: boolean = false
 
+	private subscriptions = new Subscription()
+
+	constructor(public breakpointService: BreakpointObserverService) {}
+
+	ngOnInit(): void {
+		this.subscriptions = this.breakpointService
+			.isDesktop()
+			.subscribe(b => (this.isDesktop = b))
+	}
+
+	getMap() {
+		console.log('get map')
+	}
+	ngOnDestroy(): void {
+		this.subscriptions.unsubscribe()
+	}
 }
