eea7c075250d78caca3b44027ab94224227ea106
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-07-05 15:34:09 +0600

    feat: add accordion.smth.smooothness

--- src/app/shared/ui-kit/accordion-item/accordion-item.component.ts
+++ src/app/shared/ui-kit/accordion-item/accordion-item.component.ts
@@ -5,7 +5,29 @@ import {AccordionItemDevice} from './interfaces/accordion-item.interface'
 @Component({
 	selector: 'mib-accordion-item',
 	templateUrl: './accordion-item.component.html',
-	styleUrls: ['./accordion-item.component.scss']
+	styleUrls: ['./accordion-item.component.scss'],
+	animations: [
+		trigger('slideInOut', [
+			state(
+				'in',
+				style({
+					height: '*',
+					opacity: 1,
+					display: 'flex'
+				})
+			),
+			state(
+				'out',
+				style({
+					height: '0px',
+					opacity: 0,
+					display: 'none'
+				})
+			),
+			transition('in => out', animate('400ms ease-in-out')),
+			transition('out => in', animate('400ms ease-in-out'))
+		])
+	]
 })
 export class AccordionItemComponent {
 	@Input() device: AccordionItemDevice = 'desktop'
