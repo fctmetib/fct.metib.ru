32385dad52e0ca967f5438b833b584f13d26f249
Viktor Tishchenko <9043258451vvt@gmail.com>
2024-09-17 15:51:02 +0600

    feat: delays.modal.table, dummy.data

--- src/app/shared/modules/modals/new-delays-page-modal/new-delays-page-modal.module.ts
+++ src/app/shared/modules/modals/new-delays-page-modal/new-delays-page-modal.module.ts
@@ -5,9 +5,19 @@ import {ModalModule} from 'src/app/shared/ui-kit/modal/modal.module'
 import {IconModule} from 'src/app/shared/ui-kit/ref-icon/icon.module'
 import {SpacingModule} from 'src/app/shared/ui-kit/spacing/spacing.module'
 import {ButtonModule} from 'src/app/shared/ui-kit/button/button.module'
+import {RubModule} from 'src/app/shared/pipes/rub/rub.module'
+import {MobileTablePopupModule} from 'src/app/shared/ui-kit/mobile-table-popup/mobile-table-popup.module'
 
 @NgModule({
 	declarations: [NewDelaysPageModalComponent],
-	imports: [CommonModule, ModalModule, SpacingModule, IconModule, ButtonModule]
+	imports: [
+		CommonModule,
+		ModalModule,
+		SpacingModule,
+		IconModule,
+		ButtonModule,
+		RubModule,
+		MobileTablePopupModule
+	]
 })
 export class NewDelaysPageModalModule {}
