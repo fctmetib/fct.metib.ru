8acdfa9bca0073398308ffc32493696d956ed338
MIKHAIL MARTSINYUK <martsinyuk.pl@gmail.com>
2024-09-19 17:38:43 +0300

    afsasf

--- nodeinstall.ps1
+++ nodeinstall.ps1
@@ -6,7 +6,7 @@ Write-Host "Installing packages..."
 npm install --force;
 Write-Host "Done."
 Write-Host "Building website..."
-NODE_OPTIONS="--max_old_space_size=4096" npm run-script build:ssr
+npm run-script build:ssr
 if (!(Test-Path "dist/metallinvestbank-web/index.html"))
 {
   Write-Error "Website building failed!"
