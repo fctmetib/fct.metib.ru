b9e18a57788dddad1f85d5559bce20c9072ff4d4
MIKHAIL MARTSINYUK <martsinyuk.pl@gmail.com>
2024-09-23 10:42:56 +0300

    demand improves

--- nodeinstall.ps1
+++ nodeinstall.ps1
@@ -6,7 +6,8 @@ Write-Host "Installing packages..."
 npm install --force;
 Write-Host "Done."
 Write-Host "Building website..."
-set NODE_OPTIONS="--max_old_space_size=4096" npm run build:ssr
+##set NODE_OPTIONS="--max_old_space_size=4096" 
+npm run build:ssr
 if (!(Test-Path "dist/metallinvestbank-web/index.html"))
 {
   Write-Error "Website building failed!"
