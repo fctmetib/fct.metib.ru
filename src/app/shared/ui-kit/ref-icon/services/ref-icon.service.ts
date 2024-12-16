3bef22d8709b1ba7102f05f3c3316e789e43c468
MIKHAIL MARTSINYUK <martsinyuk.pl@gmail.com>
2024-12-16 23:16:14 +0300

    try icons fix

--- src/app/shared/ui-kit/ref-icon/services/ref-icon.service.ts
+++ src/app/shared/ui-kit/ref-icon/services/ref-icon.service.ts
@@ -1,11 +1,10 @@
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { TransferState, makeStateKey, StateKey } from '@angular/platform-browser';
 import { isPlatformServer } from '@angular/common';
-import { IconsService } from 'src/app/shared/services/icons.servcie';
 
-const BASE_URL_KEY = makeStateKey<string>('BASE_URL');
 const ICONS_KEY: StateKey<{ [key: string]: string }> = makeStateKey<{ [key: string]: string }>('icons');
+
+import iconsJson from 'src/assets/icons/icons.json';
 
 @Injectable({
   providedIn: 'root'
@@ -15,9 +14,7 @@ export class RefIconService {
   private loadingIconsPromise: Promise<void> | null = null;
 
   constructor(
-    private http: HttpClient,
     private transferState: TransferState,
-	private iconsService: IconsService,
     @Inject(PLATFORM_ID) private platformId: Object
   ) {
     if (!this.isServer()) {
@@ -46,93 +43,17 @@ export class RefIconService {
     await this.loadingIconsPromise;
   }
 
-  public async initIcons(): Promise<void> {
-	try {
-		const url = `${this.getBaseUrl()}/assets/icons/icons.json`;
-		console.log('Requesting icons from:', url);
-  
-		const allIcons = await this.http
-		  .get<{ [key: string]: string }>(url)
-		  .toPromise();
-  
-		this.icons = allIcons || {};
-  
-		if (this.isServer()) {
-		  this.transferState.set(ICONS_KEY, this.icons);
-		}
-	  } catch (error) {
-		console.error('Failed to load icons:', error);
-	  }
-  }
-
   private async loadAllIcons(): Promise<void> {
 	try {
 	  const isDevMode = !this.isServer();
 	  console.log('Dev mode:', isDevMode);
   
-	  if (isDevMode) {
-		const iconsDir = '/assets/icons/ui-kit-icons/';
-		const iconNames = this.iconsService.icons;
-		console.log('Client mode: iconsDir:', iconsDir, 'iconNames:', iconNames);
-  
-		for (const iconName of iconNames) {
-		  const iconUrl = `${iconsDir}${iconName}.svg`;
-		  console.log('Fetching icon:', iconUrl);
-		  const svgContent = await this.http.get(iconUrl, { responseType: 'text' }).toPromise();
-		  console.log(`Icon fetched: ${iconName}`, svgContent ? 'OK' : 'EMPTY');
-		  this.icons[iconName] = svgContent;
-		}
-		console.log('Client. Icons loaded:', Object.keys(this.icons));
-	  } else {
-		const url = `${this.getBaseUrl()}/assets/icons/icons.json`;
-		console.log('Server mode: Fetching icons from:', url);
-
-		let response;
-		try {
-		  response = await this.http.get<{ [key: string]: string }>(url).toPromise();
-		  console.log('Server response:', response);
-		} catch (e) {
-		  console.log('Error fetching icons on server:', e);
-		}
-
-		if (!response) {
-		  console.log('No response from icons.json, allIcons is undefined');
-		} else {
-		  console.log('Server. Icons keys:', Object.keys(response));
-		}
-  
-		const allIcons = response;
-		if (allIcons && allIcons["vk"]) {
-		  console.log('Server. Icons:', allIcons["vk"]);
-		} else {
-		  console.log('Server. No vk icon in response');
-		}
-		this.icons = allIcons || {};
-	  }
-  
-	  if (this.isServer()) {
-		console.log('Setting icons to transferState...');
-		this.transferState.set(ICONS_KEY, this.icons);
-		console.log('ICONS_KEY stored:', this.transferState.get(ICONS_KEY, null)["vk"]);
-	  }
-  
+	  this.icons = iconsJson;
+	  this.transferState.set(ICONS_KEY, this.icons);
+	  console.log('ICONS', this.icons["vk"]);
 	} catch (error) {
 	  console.error('Failed to load icons:', error);
 	}
-  }  
-
-  private getBaseUrl(): string {
-	// TODO: Изменить на динамичный вид, второй параметр null
-	// const baseUrl = this.transferState.get(BASE_URL_KEY, "https://factoring.metallinvestbank.ru");
-	// if (baseUrl) {
-	//   return baseUrl;
-	// }
-  
-	if (!this.isServer() && typeof window !== 'undefined') {
-	  return window.location.origin;
-	}
-  
-	return 'https://factoring.metallinvestbank.ru';
   }  
 
   private isServer(): boolean {
